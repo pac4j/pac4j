3fe8d81df6f8aea56bb52c47a6aa58b21a7a6274
LELEU Jerome <leleuj@gmail.com>
2012-09-25 12:07:32 +0200

    handle proxy

--- src/main/java/org/scribe/up/addon_to_scribe/Google2JsonExtractor.java
+++ src/main/java/org/scribe/up/addon_to_scribe/Google2JsonExtractor.java
@@ -1,3 +1,18 @@
+/*
+  Copyright 2012 Jerome Leleu
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package org.scribe.up.addon_to_scribe;
 
 import java.util.regex.Matcher;
@@ -21,7 +36,7 @@ public class Google2JsonExtractor implements AccessTokenExtractor {
     
     public Token extract(final String response) {
         Preconditions.checkEmptyString(response, "Cannot extract a token from a null or empty String");
-        final Matcher matcher = accessTokenPattern.matcher(response);
+        final Matcher matcher = this.accessTokenPattern.matcher(response);
         if (matcher.find()) {
             return new Token(matcher.group(1), "", response);
         } else {
