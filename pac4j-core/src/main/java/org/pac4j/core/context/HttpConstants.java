4b2c6be1b3e57ecaf2a7bdffc0abf8c505bde28e
Michael Remond <michaelremond@gmail.com>
2014-07-31 14:38:53 +0200

    Pull in core some constants and config options

--- pac4j-core/src/main/java/org/pac4j/core/context/HttpConstants.java
+++ pac4j-core/src/main/java/org/pac4j/core/context/HttpConstants.java
@@ -22,20 +22,29 @@ package org.pac4j.core.context;
  * @since 1.4.0
  */
 public interface HttpConstants {
-    
+
     public static final int OK = 200;
-    
+
     public static final int UNAUTHORIZED = 401;
-    
+
     public static final int FORBIDDEN = 403;
-    
+
     public static final int TEMP_REDIRECT = 302;
-    
+
     public static final int DEFAULT_PORT = 80;
-    
+
     public static final String LOCATION_HEADER = "Location";
-    
+
     public static final String AUTHORIZATION_HEADER = "Authorization";
-    
+
     public static final String AUTHENTICATE_HEADER = "WWW-Authenticate";
+
+    public static final String CONTENT_TYPE_HEADER = "Content-Type";
+
+    public final static String HTML_CONTENT_TYPE = "text/html; charset=utf-8";
+
+    public final static String REQUESTED_URL = "pac4jRequestedUrl";
+
+    public final static String USER_PROFILE = "pac4jUserProfile";
+
 }
