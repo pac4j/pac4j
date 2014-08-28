a3859772e04462c9524e843c0f8ba3812c867fc5
Jérôme LELEU <leleuj@gmail.com>
2014-08-28 09:32:08 +0200

    update CAS client, security reasons

--- pac4j-cas/src/main/java/org/pac4j/cas/logout/CasSingleSignOutHandler.java
+++ pac4j-cas/src/main/java/org/pac4j/cas/logout/CasSingleSignOutHandler.java
@@ -15,10 +15,23 @@
  */
 package org.pac4j.cas.logout;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.jasig.cas.client.session.HashMapBackedSessionMappingStorage;
+import org.jasig.cas.client.session.SessionMappingStorage;
 import org.jasig.cas.client.session.SingleSignOutHandler;
+import org.jasig.cas.client.util.CommonUtils;
+import org.jasig.cas.client.util.XmlUtils;
 import org.pac4j.cas.client.CasClient;
 import org.pac4j.core.context.J2EContext;
 import org.pac4j.core.context.WebContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class is the logout handler for the {@link CasClient} class based on the {@link SingleSignOutHandler} class of the Jasig CAS client.<br />
@@ -29,41 +42,141 @@ import org.pac4j.core.context.WebContext;
  */
 public class CasSingleSignOutHandler implements LogoutHandler {
     
-    private final SingleSignOutHandler singleSignOutHandler;
+    protected static final Logger logger = LoggerFactory.getLogger(CasSingleSignOutHandler.class);
+
+    private SessionMappingStorage sessionMappingStorage = new HashMapBackedSessionMappingStorage();
+
+    private String artifactParameterName = SingleSignOutHandler.DEFAULT_ARTIFACT_PARAMETER_NAME;
+
+    private String logoutParameterName = SingleSignOutHandler.DEFAULT_LOGOUT_PARAMETER_NAME;
+
+    private List<String> safeParameters= Arrays.asList(this.logoutParameterName);
+    
+    private boolean eagerlyCreateSessions = true;
+
+    private LogoutStrategy logoutStrategy = isServlet30() ? new Servlet30LogoutStrategy() : new Servlet25LogoutStrategy();
     
     /**
-     * Construct an instance from the default {@link SingleSignOutHandler} class.
+     * Construct an instance.
      */
     public CasSingleSignOutHandler() {
-        this.singleSignOutHandler = new SingleSignOutHandler();
     }
     
     /**
      * Construct an instance from a given {@link SingleSignOutHandler} class.
+     * The provided handler is not taken into account.
      * 
      * @param singleSignOutHandler
      */
+    @Deprecated
     public CasSingleSignOutHandler(final SingleSignOutHandler singleSignOutHandler) {
-        this.singleSignOutHandler = singleSignOutHandler;
+        logger.warn("The provided CAS SingleSignOutHandler is not taken into account anymore!");
     }
     
     public boolean isTokenRequest(final WebContext context) {
         final J2EContext j2eContext = (J2EContext) context;
-        return this.singleSignOutHandler.isTokenRequest(j2eContext.getRequest());
+        return CommonUtils.isNotBlank(CommonUtils.safeGetParameter(j2eContext.getRequest(), this.artifactParameterName,
+                this.safeParameters));
     }
     
     public boolean isLogoutRequest(final WebContext context) {
         final J2EContext j2eContext = (J2EContext) context;
-        return this.singleSignOutHandler.isLogoutRequest(j2eContext.getRequest());
+        HttpServletRequest request = j2eContext.getRequest();
+        return "POST".equals(request.getMethod())
+                && !isMultipartRequest(request)
+                && CommonUtils.isNotBlank(CommonUtils.safeGetParameter(request, this.logoutParameterName,
+                        this.safeParameters));
     }
     
     public void recordSession(final WebContext context, final String ticket) {
         final J2EContext j2eContext = (J2EContext) context;
-        this.singleSignOutHandler.recordSession(j2eContext.getRequest());
+        HttpServletRequest request = j2eContext.getRequest();
+        final HttpSession session = request.getSession(this.eagerlyCreateSessions);
+
+        if (session == null) {
+            logger.debug("No session currently exists (and none created).  Cannot record session information for single sign out.");
+            return;
+        }
+
+        final String token = CommonUtils.safeGetParameter(request, this.artifactParameterName, this.safeParameters);
+        logger.debug("Recording session for token {}", token);
+
+        try {
+            this.sessionMappingStorage.removeBySessionById(session.getId());
+        } catch (final Exception e) {
+            // ignore if the session is already marked as invalid.  Nothing we can do!
+        }
+        sessionMappingStorage.addSessionById(token, session);
     }
     
     public void destroySession(final WebContext context) {
         final J2EContext j2eContext = (J2EContext) context;
-        this.singleSignOutHandler.destroySession(j2eContext.getRequest());
+        HttpServletRequest request = j2eContext.getRequest();
+        final String logoutMessage = CommonUtils.safeGetParameter(request, this.logoutParameterName, this.safeParameters);
+        logger.trace("Logout request:\n{}", logoutMessage);
+
+        final String token = XmlUtils.getTextForElement(logoutMessage, "SessionIndex");
+        if (CommonUtils.isNotBlank(token)) {
+            final HttpSession session = this.sessionMappingStorage.removeSessionByMappingId(token);
+
+            if (session != null) {
+                String sessionID = session.getId();
+
+                logger.debug("Invalidating session [{}] for token [{}]", sessionID, token);
+
+                try {
+                    session.invalidate();
+                } catch (final IllegalStateException e) {
+                    logger.debug("Error invalidating session.", e);
+                }
+                this.logoutStrategy.logout(request);
+            }
+        }
+    }
+
+    private boolean isMultipartRequest(final HttpServletRequest request) {
+        return request.getContentType() != null && request.getContentType().toLowerCase().startsWith("multipart");
+    }
+
+    private static boolean isServlet30() {
+        try {
+            return HttpServletRequest.class.getMethod("logout") != null;
+        } catch (final NoSuchMethodException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Abstracts the ways we can force logout with the Servlet spec.
+     */
+    private interface LogoutStrategy {
+
+        void logout(HttpServletRequest request);
+    }
+
+    private class Servlet25LogoutStrategy implements LogoutStrategy {
+
+        public void logout(final HttpServletRequest request) {
+            // nothing additional to do here
+        }
+    }
+
+    private class Servlet30LogoutStrategy implements LogoutStrategy {
+
+        public void logout(final HttpServletRequest request) {
+            try {
+                request.logout();
+            } catch (final ServletException e) {
+                logger.debug("Error performing request.logout.");
+            }
+        }
+    }
+
+    public SessionMappingStorage getSessionMappingStorage() {
+        return sessionMappingStorage;
+    }
+
+    public void setSessionMappingStorage(SessionMappingStorage sessionMappingStorage) {
+        this.sessionMappingStorage = sessionMappingStorage;
     }
 }
