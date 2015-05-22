3e4a4204a62b730f901618db7b443d582be3212d
Misagh Moayyed <mm1844@gmail.com>
2015-05-23 01:43:15 +0400

    fixed compile issues

--- pac4j-saml/src/main/java/org/pac4j/saml/sso/Saml2ResponseValidator.java
+++ pac4j-saml/src/main/java/org/pac4j/saml/sso/Saml2ResponseValidator.java
@@ -4,8 +4,12 @@ import org.pac4j.core.credentials.Credentials;
 import org.pac4j.saml.context.ExtendedSAMLMessageContext;
 
 /**
+ * Defines operations needed to validate the response
+ * from IdP.
  * @author Misagh Moayyed
+ * @since 1.7
  */
 public interface SAML2ResponseValidator {
+
     Credentials validate(ExtendedSAMLMessageContext context);
 }
