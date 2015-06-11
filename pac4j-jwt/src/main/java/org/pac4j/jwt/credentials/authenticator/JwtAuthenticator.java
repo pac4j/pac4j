55715d8f8d36095a3abaf7ac0eccd1e274b08b55
Jérôme LELEU <jerome.leleu@sfr.com>
2015-06-11 14:35:42 +0200

    first implem

---
+++ pac4j-jwt/src/main/java/org/pac4j/jwt/credentials/authenticator/JwtAuthenticator.java
@@ -0,0 +1,91 @@
+/*
+  Copyright 2012 - 2015 pac4j organization
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package org.pac4j.jwt.credentials.authenticator;
+
+import com.nimbusds.jose.JWEObject;
+import com.nimbusds.jose.crypto.DirectDecrypter;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.ReadOnlyJWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import org.pac4j.core.exception.CredentialsException;
+import org.pac4j.core.exception.TechnicalException;
+import org.pac4j.http.credentials.TokenCredentials;
+import org.pac4j.http.credentials.authenticator.TokenAuthenticator;
+import org.pac4j.jwt.profile.JwtProfile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Authenticator for JWT. It creates a {@link org.pac4j.jwt.profile.JwtProfile} and
+ * stores it in the credentials for the {@link org.pac4j.http.profile.creator.AuthenticatorProfileCreator}.
+ *
+ * @author Jerome Leleu
+ * @since 1.8.0
+ */
+public class JwtAuthenticator implements TokenAuthenticator {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final String secret;
+
+    public JwtAuthenticator(final String secret) {
+        this.secret = secret;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void validate(TokenCredentials credentials) {
+
+        final String token = credentials.getToken();
+        boolean verified = false;
+        SignedJWT signedJWT = null;
+
+        try {
+            // Parse the JWE string
+            JWEObject jweObject = JWEObject.parse(token);
+
+            // Decrypt with shared key
+            jweObject.decrypt(new DirectDecrypter(this.secret.getBytes("UTF-8")));
+
+            // Extract payload
+            signedJWT = jweObject.getPayload().toSignedJWT();
+
+            verified = signedJWT.verify(new MACVerifier(this.secret));
+        } catch (final Exception e) {
+            logger.error("Cannot decrypt / verify JWT", e);
+            throw new TechnicalException("Cannot decrypt / verify JWT: " + e.getMessage());
+        }
+
+        if (!verified) {
+            final String message = "JWT verification failed: " + token;
+            logger.error(message);
+            throw new CredentialsException(message);
+        }
+
+        try {
+            final ReadOnlyJWTClaimsSet claimSet = signedJWT.getJWTClaimsSet();
+            final JwtProfile profile = new JwtProfile();
+            profile.setId(claimSet.getSubject());
+            profile.addAttributes(claimSet.getCustomClaims());
+            credentials.setUserProfile(profile);
+        } catch (final Exception e) {
+            logger.error("Cannot get claimSet", e);
+            throw new TechnicalException("Cannot get claimSet: " + e.getMessage());
+        }
+    }
+}
---
+++ pac4j-jwt/src/main/java/org/pac4j/jwt/profile/JwtGenerator.java
@@ -0,0 +1,86 @@
+/*
+  Copyright 2012 - 2015 pac4j organization
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package org.pac4j.jwt.profile;
+
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.DirectEncrypter;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import org.pac4j.core.exception.TechnicalException;
+import org.pac4j.core.profile.UserProfile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+/**
+ * Generates a JWT token from a user profile.
+ *
+ * @author Jerome Leleu
+ * @since 1.8.0
+ */
+public class JwtGenerator<U extends UserProfile> {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final String secret;
+
+    public JwtGenerator(final String secret) {
+        this.secret = secret;
+    }
+
+    /**
+     * Generates a JWT from a user profile.
+     *
+     * @param profile the given user profile
+     * @return the created JWT
+     */
+    public String generate(final U profile) {
+
+        try {
+            // Create HMAC signer
+            JWSSigner signer = new MACSigner(this.secret);
+
+            // Prepare JWT with claims set
+            JWTClaimsSet claimsSet = new JWTClaimsSet();
+            claimsSet.setSubject(profile.getId());
+            claimsSet.setIssueTime(new Date());
+            claimsSet.setIssuer(this.getClass().getSimpleName());
+            claimsSet.setCustomClaims(profile.getAttributes());
+
+            SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+
+            // Apply the HMAC
+            signedJWT.sign(signer);
+
+            // Create JWE object with signed JWT as payload
+            JWEObject jweObject = new JWEObject(
+                    new JWEHeader.Builder(JWEAlgorithm.DIR, EncryptionMethod.A256GCM).contentType("JWT").build(),
+                    new Payload(signedJWT));
+
+            // Perform encryption
+            jweObject.encrypt(new DirectEncrypter(this.secret.getBytes("UTF-8")));
+
+            // Serialise to JWE compact form
+            return jweObject.serialize();
+
+        } catch (final Exception e) {
+            logger.error("Cannot generate JWT", e);
+            throw new TechnicalException("Cannot generate JWT: " + e.getMessage());
+        }
+    }
+}
