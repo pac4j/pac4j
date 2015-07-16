d1afbff4ec2e729d8d6a1ed11a5211155c45b490
Jérôme LELEU <jerome.leleu@sfr.com>
2015-07-16 16:21:25 +0200

    ldap authent

--- pac4j-ldap/src/main/java/org/pac4j/ldap/credentials/authenticator/LdapAuthenticator.java
+++ pac4j-ldap/src/main/java/org/pac4j/ldap/credentials/authenticator/LdapAuthenticator.java
@@ -15,10 +15,20 @@
  */
 package org.pac4j.ldap.credentials.authenticator;
 
+import org.ldaptive.LdapAttribute;
+import org.ldaptive.LdapEntry;
+import org.ldaptive.LdapException;
+import org.ldaptive.auth.AuthenticationRequest;
+import org.ldaptive.auth.AuthenticationResponse;
+import org.ldaptive.auth.AuthenticationResultCode;
 import org.ldaptive.auth.Authenticator;
+import org.pac4j.core.exception.AccountNotFoundException;
+import org.pac4j.core.exception.BadCredentialsException;
+import org.pac4j.core.exception.TechnicalException;
 import org.pac4j.core.util.CommonHelper;
 import org.pac4j.http.credentials.UsernamePasswordCredentials;
 import org.pac4j.http.credentials.authenticator.UsernamePasswordAuthenticator;
+import org.pac4j.ldap.profile.LdapProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,6 +61,49 @@ public class LdapAuthenticator implements UsernamePasswordAuthenticator {
 
     public void validate(UsernamePasswordCredentials credentials) {
         CommonHelper.assertNotNull("ldapAuthenticator", ldapAuthenticator);
+        CommonHelper.assertNotNull("attributes", attributes);
+
+        final String username = credentials.getUsername();
+        final String[] ldapAttributes = attributes.split(",");
+        final AuthenticationResponse response;
+        try {
+            logger.debug("Attempting LDAP authentication for {}", credentials);
+            final AuthenticationRequest request = new AuthenticationRequest(username,
+                    new org.ldaptive.Credential(credentials.getPassword()),
+                    ldapAttributes);
+            response = this.ldapAuthenticator.authenticate(request);
+        } catch (final LdapException e) {
+            throw new TechnicalException("Unexpected LDAP error", e);
+        }
+        logger.debug("LDAP response: {}", response);
+
+        if (response.getResult()) {
+            final LdapProfile profile = createProfile(username, ldapAttributes, response.getLdapEntry());
+            credentials.setUserProfile(profile);
+            return;
+        }
+
+        if (AuthenticationResultCode.DN_RESOLUTION_FAILURE == response.getAuthenticationResultCode()) {
+            throw new AccountNotFoundException(username + " not found.");
+        }
+        throw new BadCredentialsException("Invalid credentials for: " + username);
+    }
+
+    protected LdapProfile createProfile(final String username, final String[] ldapAttributes, final LdapEntry entry) {
+        final LdapProfile profile = new LdapProfile();
+        profile.setId(username);
+        for (String ldapAttribute: ldapAttributes) {
+            final LdapAttribute entryAttribute = entry.getAttribute(ldapAttribute);
+            if (entryAttribute != null) {
+                logger.debug("Found attribute: {}", ldapAttribute);
+                if (entryAttribute.size() > 1) {
+                    profile.addAttribute(ldapAttribute, entryAttribute.getStringValues());
+                } else {
+                    profile.addAttribute(ldapAttribute, entryAttribute.getStringValue());
+                }
+            }
+        }
+        return profile;
     }
 
     public Authenticator getLdapAuthenticator() {
---
+++ pac4j-ldap/src/test/java/org/pac4j/ldap/test/LdapServer.java
@@ -0,0 +1,68 @@
+/*
+  Copyright 2012 - 2015 pac4j organization
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package org.pac4j.ldap.test;
+
+import com.unboundid.ldap.listener.InMemoryDirectoryServer;
+import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig;
+import com.unboundid.ldap.listener.InMemoryListenerConfig;
+
+/**
+ * Simulates a basic LDAP server.
+ *
+ * @author Jerome Leleu
+ * @since 1.8.0
+ */
+public class LdapServer {
+
+    public final static String BASE_DN = "dc=example,dc=com";
+    public final static String BASE_PEOPLE_DN = "ou=people,dc=example,dc=com";
+    public final static int PORT = 33389;
+    public final static String CN = "cn";
+    public final static String SN = "sn";
+    public final static String USERNAME = "jle";
+    public final static String USERNAME2 = "jleleu";
+    public final static String PASSWORD = "password";
+    public final static String FIRSTNAME = "Jerome";
+    public final static String ROLE = "role";
+    public final static String ROLE1 = "role1";
+    public final static String ROLE2 = "role2";
+
+    private InMemoryDirectoryServer ds;
+
+    public void start() throws Exception {
+        final InMemoryDirectoryServerConfig dsConfig = new InMemoryDirectoryServerConfig(BASE_DN);
+        dsConfig.setSchema(null);
+        dsConfig.setEnforceAttributeSyntaxCompliance(false);
+        dsConfig.setEnforceSingleStructuralObjectClass(false);
+        dsConfig.setListenerConfigs(new InMemoryListenerConfig("myListener", null, PORT, null, null, null));
+        dsConfig.addAdditionalBindCredentials(CN + "=" + USERNAME + "," + BASE_PEOPLE_DN, PASSWORD);
+        this.ds = new InMemoryDirectoryServer(dsConfig);
+        this.ds.add("dn: " + BASE_DN, "objectClass: organizationalUnit", "objectClass: top");
+        this.ds.add("dn: " + BASE_PEOPLE_DN, "objectClass: organizationalUnit");
+        this.ds.add("dn: " + CN + "=" + USERNAME + "," + BASE_PEOPLE_DN, CN + ": " + USERNAME, SN + ": "
+                + FIRSTNAME, "objectClass: person");
+        this.ds.add("dn: " + CN + "=" + USERNAME2 + "," + BASE_PEOPLE_DN, ROLE + ": " + ROLE1, ROLE + ": " + ROLE2,
+                "objectClass: person");
+
+        //Debug.setEnabled(true);
+
+        this.ds.startListening();
+    }
+
+    public void stop() {
+        this.ds.shutDown(true);
+    }
+}
