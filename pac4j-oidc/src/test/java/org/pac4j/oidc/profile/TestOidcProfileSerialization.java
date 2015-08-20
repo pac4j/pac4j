4441e7c67d3bf6b5a9814a86470d0a431c507e3f
Tom Hanks <thanks@mymatrixx.com>
2015-08-20 19:25:35 -0400

    Alter the test here to cover `idTokenString` as well.

--- pac4j-oidc/src/test/java/org/pac4j/oidc/profile/TestOidcProfileSerialization.java
+++ pac4j-oidc/src/test/java/org/pac4j/oidc/profile/TestOidcProfileSerialization.java
@@ -17,8 +17,11 @@ public class TestOidcProfileSerialization {
     @Test
     public void testReadWriteObject() throws Exception {
 
+        String idToken = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJpc3MiOiJodHRwczovL2p3dC1pZHAuZXhhbXBsZS5jb20iLCJzdWIiOiJtYWlsdG86cGVyc29uQGV4YW1wbGUuY29tIiwibmJmIjoxNDQwMTEyMDE1LCJleHAiOjE0NDAxMTU2MTUsImlhdCI6MTQ0MDExMjAxNSwianRpIjoiaWQxMjM0NTYiLCJ0eXAiOiJodHRwczovL2V4YW1wbGUuY29tL3JlZ2lzdGVyIn0.";
         BearerAccessToken token = new BearerAccessToken(32, 128, Scope.parse("oidc email"));
         OidcProfile profile = new OidcProfile(token);
+        profile.setIdTokenString(idToken);
+
         byte[] result = SerializationUtils.serialize(profile);
 
         profile = (OidcProfile) SerializationUtils.deserialize(result);
@@ -28,6 +31,7 @@ public class TestOidcProfileSerialization {
                 "lifetimes do not match");
         CommonHelper.assertTrue(profile.getAccessToken().getScope().equals(token.getScope()),
                 "scopes do not match");
+        CommonHelper.assertTrue(idToken.equals(profile.getIdTokenString()), "id tokens do not match");
 
     }
 }
