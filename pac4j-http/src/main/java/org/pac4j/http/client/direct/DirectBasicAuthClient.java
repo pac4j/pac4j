5e78d9bba355e7ad61f13e2ae052e0ccec222c5a
Jérôme LELEU <jerome.leleu@sfr.com>
2015-08-31 11:30:38 +0200

    Deprecate BasicAuthClient

--- pac4j-http/src/main/java/org/pac4j/http/client/direct/DirectBasicAuthClient.java
+++ pac4j-http/src/main/java/org/pac4j/http/client/direct/DirectBasicAuthClient.java
@@ -30,17 +30,17 @@ import org.pac4j.http.profile.creator.ProfileCreator;
  * @author Jerome Leleu
  * @since 1.8.0
  */
-public class BasicAuthClient extends DirectHttpClient<UsernamePasswordCredentials> {
+public class DirectBasicAuthClient extends DirectHttpClient<UsernamePasswordCredentials> {
 
     private String headerName = HttpConstants.AUTHORIZATION_HEADER;
 
     private String prefixHeader = "Basic ";
 
-    public BasicAuthClient() {
+    public DirectBasicAuthClient() {
     }
 
-    public BasicAuthClient(final UsernamePasswordAuthenticator usernamePasswordAuthenticator,
-                           final ProfileCreator profilePopulator) {
+    public DirectBasicAuthClient(final UsernamePasswordAuthenticator usernamePasswordAuthenticator,
+                                 final ProfileCreator profilePopulator) {
         setAuthenticator(usernamePasswordAuthenticator);
         setProfileCreator(profilePopulator);
     }
@@ -52,8 +52,8 @@ public class BasicAuthClient extends DirectHttpClient<UsernamePasswordCredential
     }
 
     @Override
-    protected BasicAuthClient newClient() {
-        return new BasicAuthClient();
+    protected DirectBasicAuthClient newClient() {
+        return new DirectBasicAuthClient();
     }
 
     @Override
