0ee664fde7195b73a7c7f5d6700d0f4cb334b4bb
Jérôme LELEU <leleuj@gmail.com>
2015-09-17 09:59:32 +0200

    add equals / hashCode on credentials to make LocalCachingAuthenticator work

--- pac4j-http/src/main/java/org/pac4j/http/credentials/UsernamePasswordCredentials.java
+++ pac4j-http/src/main/java/org/pac4j/http/credentials/UsernamePasswordCredentials.java
@@ -46,6 +46,25 @@ public class UsernamePasswordCredentials extends HttpCredentials {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsernamePasswordCredentials that = (UsernamePasswordCredentials) o;
+
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        return !(password != null ? !password.equals(that.password) : that.password != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return CommonHelper.toString(this.getClass(), "username", this.username, "password", "[PROTECTED]",
                 "clientName", getClientName());
