b93ec7b5669e487198d66a1cc16300a588b475c6
LELEU Jérôme <leleuj@gmail.com>
2015-10-05 12:58:28 +0200

    Update UriUtils.java

--- pac4j-saml/src/main/java/org/pac4j/saml/util/UriUtils.java
+++ pac4j-saml/src/main/java/org/pac4j/saml/util/UriUtils.java
@@ -1,4 +1,16 @@
-package org.pac4j.saml.util;
+/*
+  Copyright 2012 -2014 pac4j organization
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+ package org.pac4j.saml.util;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -11,6 +23,7 @@ import org.slf4j.LoggerFactory;
  * URI utilities.
  * 
  * @author jkacer
+ * @since 1.8.0
  */
 public class UriUtils {
 
