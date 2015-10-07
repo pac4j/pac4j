a8c4fd46088f81befa0dea9cc7f3d85789c148a9
Jérôme LELEU <leleuj@gmail.com>
2015-10-07 18:15:41 +0200

    Removing any dependency on J2E elements

--- pac4j-saml/src/main/java/org/pac4j/saml/client/SAML2Client.java
+++ pac4j-saml/src/main/java/org/pac4j/saml/client/SAML2Client.java
@@ -13,7 +13,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-
 package org.pac4j.saml.client;
 
 import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
