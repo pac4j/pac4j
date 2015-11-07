8b93d102360871ab3ebf2df0d786e0fce5037526
Jérôme LELEU <leleuj@gmail.com>
2015-11-07 14:50:04 +0100

    replace contextualcallback by a specific resolver

--- pac4j-openid/src/main/java/org/pac4j/openid/client/BaseOpenIdClient.java
+++ pac4j-openid/src/main/java/org/pac4j/openid/client/BaseOpenIdClient.java
@@ -102,10 +102,9 @@ public abstract class BaseOpenIdClient<U extends CommonProfile> extends Indirect
             // save discovery information in session
             context.setSessionAttribute(getDiscoveryInformationSessionAttributeName(), discoveryInformation);
 
-            final String contextualCallbackUrl = getContextualCallbackUrl(context);
             // create authentication request to be sent to the OpenID provider
             final AuthRequest authRequest = this.consumerManager.authenticate(discoveryInformation,
-                    contextualCallbackUrl);
+                    computeFinalCallbackUrl(context));
 
             // create fetch request for attributes
             final FetchRequest fetchRequest = getFetchRequest();
@@ -165,9 +164,8 @@ public abstract class BaseOpenIdClient<U extends CommonProfile> extends Indirect
         logger.debug("discoveryInformation : {}", discoveryInformation);
 
         try {
-            final String contextualCallbackUrl = getContextualCallbackUrl(context);
             // verify the response
-            final VerificationResult verification = this.consumerManager.verify(contextualCallbackUrl, parameterList,
+            final VerificationResult verification = this.consumerManager.verify(computeFinalCallbackUrl(context), parameterList,
                     discoveryInformation);
 
             // examine the verification result and extract the verified identifier
