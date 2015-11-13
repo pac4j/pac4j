b44094046bdc8cea0c11882f6ee8016eb5392adc
Jarda Kacer <jaroslav@kacer.biz>
2015-11-13 17:16:46 +0100

    Database config of SAML client - refactoring after code review
    Added license headers everywhere.

--- pac4j-saml-config-db/src/test/resources/org/pac4j/saml/dbclient/dao/impl/2_Fill_Data.sql
+++ pac4j-saml-config-db/src/test/resources/org/pac4j/saml/dbclient/dao/impl/2_Fill_Data.sql
@@ -1,3 +1,19 @@
+--
+--   Copyright 2012 - 2015 pac4j organization
+--
+--   Licensed under the Apache License, Version 2.0 (the "License");
+--   you may not use this file except in compliance with the License.
+--   You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--   Unless required by applicable law or agreed to in writing, software
+--   distributed under the License is distributed on an "AS IS" BASIS,
+--   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--   See the License for the specific language governing permissions and
+--   limitations under the License.
+--
+
 insert into PAC4J_CFG(CLIENT_NAME, ENVIRONMENT, KEYSTORE_DATA, KEYSTORE_PASSWORD, PRIVATE_KEY_PASSWORD, IDP_METADATA, IDP_ENTITY_ID, SP_ENTITY_ID, MAX_AUTH_LIFETIME, DEST_BINDING_TYPE)
 values ('One', 'UnitTest', X'0101010101', 'KsPwd1', 'PrKeyPwd1', 'CLOB-1', 'urn:idp1', 'urn:sp1', 1000, NULL);
 
