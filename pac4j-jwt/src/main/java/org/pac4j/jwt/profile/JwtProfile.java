7d12b975b4d400e794b0606efbdd5db1f2e91625
Misagh Moayyed <mmoayyed@unicon.net>
2015-12-16 01:29:13 -0700

    switch to using HttpProfile for JWT

--- pac4j-jwt/src/main/java/org/pac4j/jwt/profile/JwtProfile.java
+++ pac4j-jwt/src/main/java/org/pac4j/jwt/profile/JwtProfile.java
@@ -1,6 +1,6 @@
 package org.pac4j.jwt.profile;
 
-import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.http.profile.HttpProfile;
 
 /**
  * Represents a user profile based on a JWT.
@@ -8,6 +8,6 @@ import org.pac4j.core.profile.CommonProfile;
  * @author Misagh Moayyed
  * @since 1.8.2
  */
-public class JwtProfile extends CommonProfile {
+public class JwtProfile extends HttpProfile {
     private static final long serialVersionUID = -1688563185891542198L;
 }
