03d1eade7e3606a309bfd71d78757e6470a7ebda
jhegg <jhegg@tripwire.com>
2016-02-24 15:19:09 -0800

    Default to require assertions to be signed
    This changes the default behavior of SAML2DefaultResponseValidator such that
    assertions must be signed, unless the user changes the default configuration in
    SAML2ClientConfiguration, or the SPSSODescriptor has `wantAssertionsSigned` set
    to false (based on the SP metadata).
    
    The reason why is because this is a security issue where an attacker can forge
    a SAML response that does not contain assertion signatures, and they can then
    specify any assertions they want. For example, they can specify a different
    name or email or group to elevate privileges. This is only possible if the
    SPSSODescriptor in the validator is null, but in our specific way of using
    pac4j-saml, that seems to always be the case at the time of validating the SAML
    response.
    
    Issue #502 SAML2DefaultResponseValidator wantAssertionsSigned cannot evaluate to true

---
+++ pac4j-saml/src/test/java/org/pac4j/saml/sso/impl/SAML2DefaultResponseValidatorTest.java
@@ -0,0 +1,115 @@
+/*
+  Copyright 2012 - 2016 pac4j organization
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package org.pac4j.saml.sso.impl;
+
+import org.junit.Test;
+import org.opensaml.saml.common.messaging.context.SAMLMetadataContext;
+import org.opensaml.saml.common.messaging.context.SAMLPeerEntityContext;
+import org.opensaml.saml.saml2.encryption.Decrypter;
+import org.opensaml.saml.saml2.metadata.SPSSODescriptor;
+import org.pac4j.saml.context.SAML2MessageContext;
+import org.pac4j.saml.crypto.SAML2SignatureTrustEngineProvider;
+import org.pac4j.saml.exceptions.SAMLException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SAML2DefaultResponseValidatorTest {
+
+  @Test
+  public void testDoesNotWantAssertionsSignedWithNullContext() throws Exception {
+    SAML2DefaultResponseValidator validator = createResponseValidatorWithSigningValidationOf(false);
+    assertFalse("Expected wantAssertionsSigned == false", validator.wantsAssertionsSigned(null));
+  }
+
+  private SAML2DefaultResponseValidator createResponseValidatorWithSigningValidationOf(boolean wantsAssertionsSigned) {
+    SAML2SignatureTrustEngineProvider trustEngineProvider = mock(SAML2SignatureTrustEngineProvider.class);
+    Decrypter decrypter = mock(Decrypter.class);
+    return new SAML2DefaultResponseValidator(trustEngineProvider, decrypter, 0, wantsAssertionsSigned);
+  }
+
+  @Test
+  public void testWantsAssertionsSignedWithNullContext() throws Exception {
+    SAML2DefaultResponseValidator validator = createResponseValidatorWithSigningValidationOf(true);
+    assertTrue("Expected wantAssertionsSigned == true", validator.wantsAssertionsSigned(null));
+  }
+
+  @Test
+  public void testDoesNotWantAssertionsSignedWithNullSPSSODescriptor() throws Exception {
+    SAML2DefaultResponseValidator validator = createResponseValidatorWithSigningValidationOf(false);
+    SAML2MessageContext context = new SAML2MessageContext();
+    assertNull("Expected SPSSODescriptor to be null", context.getSPSSODescriptor());
+    assertFalse("Expected wantAssertionsSigned == false", validator.wantsAssertionsSigned(context));
+  }
+
+  @Test
+  public void testWantsAssertionsSignedWithNullSPSSODescriptor() throws Exception {
+    SAML2DefaultResponseValidator validator = createResponseValidatorWithSigningValidationOf(true);
+    SAML2MessageContext context = new SAML2MessageContext();
+    assertNull("Expected SPSSODescriptor to be null", context.getSPSSODescriptor());
+    assertTrue("Expected wantAssertionsSigned == true", validator.wantsAssertionsSigned(context));
+  }
+
+  @Test
+  public void testDoesNotWantAssertionsSignedWithValidSPSSODescriptor() throws Exception {
+    SAML2DefaultResponseValidator validator = createResponseValidatorWithSigningValidationOf(false);
+    SAML2MessageContext context = new SAML2MessageContext();
+
+    SAMLMetadataContext samlSelfMetadataContext = context.getSAMLSelfMetadataContext();
+    SPSSODescriptor roleDescriptor = mock(SPSSODescriptor.class);
+    when(roleDescriptor.getWantAssertionsSigned()).thenReturn(false);
+    samlSelfMetadataContext.setRoleDescriptor(roleDescriptor);
+
+    assertNotNull("Expected SPSSODescriptor to not be null", context.getSPSSODescriptor());
+    assertFalse("Expected wantAssertionsSigned == false", validator.wantsAssertionsSigned(context));
+  }
+
+  @Test
+  public void testWantsAssertionsSignedWithValidSPSSODescriptor() throws Exception {
+    SAML2DefaultResponseValidator validator = createResponseValidatorWithSigningValidationOf(true);
+    SAML2MessageContext context = new SAML2MessageContext();
+
+    SAMLMetadataContext samlSelfMetadataContext = context.getSAMLSelfMetadataContext();
+    SPSSODescriptor roleDescriptor = mock(SPSSODescriptor.class);
+    when(roleDescriptor.getWantAssertionsSigned()).thenReturn(true);
+    samlSelfMetadataContext.setRoleDescriptor(roleDescriptor);
+
+    assertNotNull("Expected SPSSODescriptor to not be null", context.getSPSSODescriptor());
+    assertTrue("Expected wantAssertionsSigned == true", validator.wantsAssertionsSigned(context));
+  }
+
+  @Test(expected = SAMLException.class)
+  public void testAssertionWithoutSignatureThrowsException() throws Exception {
+    SAML2DefaultResponseValidator validator = createResponseValidatorWithSigningValidationOf(true);
+    SAML2MessageContext context = new SAML2MessageContext();
+    SAMLPeerEntityContext peerEntityContext = new SAMLPeerEntityContext();
+    peerEntityContext.setAuthenticated(false);
+    context.addSubcontext(peerEntityContext);
+    validator.validateAssertionSignature(null, context, null);
+  }
+
+  @Test
+  public void testAssertionWithoutSignatureDoesNotThrowException() throws Exception {
+    SAML2DefaultResponseValidator validator = createResponseValidatorWithSigningValidationOf(false);
+    SAML2MessageContext context = new SAML2MessageContext();
+    SAMLPeerEntityContext peerEntityContext = new SAMLPeerEntityContext();
+    peerEntityContext.setAuthenticated(false);
+    context.addSubcontext(peerEntityContext);
+    validator.validateAssertionSignature(null, context, null);
+    // expected no exceptions
+  }
+}
