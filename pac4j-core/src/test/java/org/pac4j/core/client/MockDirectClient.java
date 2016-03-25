c7d97c6d60053433b338ec544ffeafb4144c80ec
Jérôme LELEU <leleuj@gmail.com>
2016-03-25 08:37:03 +0100

    Add mocks for tests in implementations

---
+++ pac4j-core/src/test/java/org/pac4j/core/client/MockDirectClient.java
@@ -0,0 +1,42 @@
+package org.pac4j.core.client;
+
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.credentials.Credentials;
+import org.pac4j.core.exception.RequiresHttpAction;
+import org.pac4j.core.profile.CommonProfile;
+
+/**
+ * Mock an indirect client.
+ *
+ * @author Jerome Leleu
+ * @since 1.9.0
+ */
+public final class MockDirectClient extends DirectClient<Credentials, CommonProfile> {
+
+    private final ReturnCredentials returnCredentials;
+
+    private final CommonProfile profile;
+
+    public MockDirectClient(final String name, final Credentials credentials, final CommonProfile profile) {
+        this(name, () -> credentials, profile);
+    }
+
+    public MockDirectClient(final String name, final ReturnCredentials returnCredentials, final CommonProfile profile) {
+        setName(name);
+        this.returnCredentials = returnCredentials;
+        this.profile = profile;
+    }
+
+    @Override
+    protected void internalInit(final WebContext context) {}
+
+    @Override
+    public Credentials getCredentials(WebContext context) throws RequiresHttpAction {
+        return returnCredentials.get();
+    }
+
+    @Override
+    protected CommonProfile retrieveUserProfile(final Credentials credentials, final WebContext context) throws RequiresHttpAction {
+        return profile;
+    }
+}
--- pac4j-http/src/main/java/org/pac4j/http/client/direct/DirectBasicAuthClient.java
+++ pac4j-http/src/main/java/org/pac4j/http/client/direct/DirectBasicAuthClient.java
@@ -1,6 +1,6 @@
 package org.pac4j.http.client.direct;
 
-import org.pac4j.core.client.DirectClient2;
+import org.pac4j.core.client.DirectClientV2;
 import org.pac4j.core.context.WebContext;
 import org.pac4j.core.credentials.UsernamePasswordCredentials;
 import org.pac4j.core.credentials.authenticator.Authenticator;
@@ -15,7 +15,7 @@ import org.pac4j.core.profile.creator.ProfileCreator;
  * @author Jerome Leleu
  * @since 1.8.0
  */
-public class DirectBasicAuthClient extends DirectClient2<UsernamePasswordCredentials, CommonProfile> {
+public class DirectBasicAuthClient extends DirectClientV2<UsernamePasswordCredentials, CommonProfile> {
 
     public DirectBasicAuthClient() {}
 
