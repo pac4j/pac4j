b243335560a30afc2651f03941f90f6cf7fac91e
Paul O'Riordan <paul_oriordan@trimble.com>
2016-04-28 15:30:37 +0100

    Fixes NPE in OidcClient when token endpoint auth methods are undefined.
    The OIDCProviderMetadata class from nimbus/oauth-oidc returns null, rather than an empty
    list when these are not specified.
    
    	/*
    	 * @return The supported token endpoint authentication methods,
    	 *         {@code null} if not specified.
    	 */
    	public List<ClientAuthenticationMethod> getTokenEndpointAuthMethods() {

--- pac4j-oidc/src/main/java/org/pac4j/oidc/client/OidcClient.java
+++ pac4j-oidc/src/main/java/org/pac4j/oidc/client/OidcClient.java
@@ -283,7 +283,7 @@ public class OidcClient<U extends OidcProfile> extends IndirectClient<OidcCreden
         final List<ClientAuthenticationMethod> methods = getProviderMetadata().getTokenEndpointAuthMethods();
         final ClientAuthenticationMethod method;
 
-        if (!methods.isEmpty()) {
+        if (methods != null && !methods.isEmpty()) {
             if (methods.contains(getClientAuthenticationMethod())) {
                 method = getClientAuthenticationMethod();
             } else {
