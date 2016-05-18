ed296f7bc91765596b08cb5796fbeeeec47db42a
Garry <garry@cambridgesemantics.com>
2016-05-18 10:45:36 -0400

    Use underlying library to decide which verifier to use based on header. Allow secret to be a PEM with associated algorithm #454

--- pac4j-jwt/src/main/java/org/pac4j/jwt/credentials/authenticator/JwtAuthenticator.java
+++ pac4j-jwt/src/main/java/org/pac4j/jwt/credentials/authenticator/JwtAuthenticator.java
@@ -16,8 +16,11 @@
 package org.pac4j.jwt.credentials.authenticator;
 
 import com.nimbusds.jose.JWEObject;
+import com.nimbusds.jose.JWSVerifier;
 import com.nimbusds.jose.crypto.DirectDecrypter;
-import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jose.crypto.factories.DefaultJWSVerifierFactory;
+import com.nimbusds.jose.proc.JWSVerifierFactory;
+import com.nimbusds.jose.util.X509CertUtils;
 import com.nimbusds.jwt.EncryptedJWT;
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTClaimsSet;
@@ -36,10 +39,18 @@ import org.pac4j.jwt.profile.JwtProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.charset.Charset;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
 import java.text.ParseException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Authenticator for JWT. It creates the user profile and stores it in the credentials
@@ -52,8 +63,11 @@ public class JwtAuthenticator implements TokenAuthenticator {
 
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private String signingSecret;
     private String encryptionSecret;
+    
+    private JWSVerifierFactory factory = new DefaultJWSVerifierFactory();
+
+	private Key key;
 
     public JwtAuthenticator() {}
 
@@ -70,7 +84,17 @@ public class JwtAuthenticator implements TokenAuthenticator {
      * @since 1.8.2
      */
     public JwtAuthenticator(final String signingSecret, final String encryptionSecret) {
-        this.signingSecret = signingSecret;
+    	setSigningSecret(signingSecret);
+        this.encryptionSecret = encryptionSecret;
+    }
+    
+    /**
+     * @throws InvalidKeySpecException 
+     * @throws NoSuchAlgorithmException 
+     * @since 1.8.2
+     */
+    public JwtAuthenticator(final String publicKeyPEM, final String algorithm, final String encryptionSecret) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    	setSigningPem(publicKeyPEM, algorithm);
         this.encryptionSecret = encryptionSecret;
     }
 
@@ -91,7 +115,7 @@ public class JwtAuthenticator implements TokenAuthenticator {
      */
     @Override
     public void validate(final TokenCredentials credentials) {
-        CommonHelper.assertNotBlank("signingSecret", signingSecret);
+        CommonHelper.assertNotNull("key", this.key);
 
         final String token = credentials.getToken();
         boolean verified = false;
@@ -114,8 +138,10 @@ public class JwtAuthenticator implements TokenAuthenticator {
             } else {
                 throw new TechnicalException("unsupported unsecured jwt");
             }
+            
+    		JWSVerifier verifier = factory.createJWSVerifier(signedJWT.getHeader(), key);
 
-            verified = signedJWT.verify(new MACVerifier(this.signingSecret));
+            verified = signedJWT.verify(verifier);
         } catch (final Exception e) {
             throw new TechnicalException("Cannot decrypt / verify JWT", e);
         }
@@ -142,9 +168,11 @@ public class JwtAuthenticator implements TokenAuthenticator {
 
         final Map<String, Object> attributes = new HashMap<>(claimSet.getClaims());
         attributes.remove(JwtConstants.SUBJECT);
-        final List<String> roles = (List<String>) attributes.get(JwtGenerator.INTERNAL_ROLES);
+        @SuppressWarnings("unchecked")
+		final List<String> roles = (List<String>) attributes.get(JwtGenerator.INTERNAL_ROLES);
         attributes.remove(JwtGenerator.INTERNAL_ROLES);
-        final List<String> permissions = (List<String>) attributes.get(JwtGenerator.INTERNAL_PERMISSIONS);
+        @SuppressWarnings("unchecked")
+		final List<String> permissions = (List<String>) attributes.get(JwtGenerator.INTERNAL_PERMISSIONS);
         attributes.remove(JwtGenerator.INTERNAL_PERMISSIONS);
         final UserProfile profile = ProfileHelper.buildProfile(subject, attributes);
         if (roles != null) {
@@ -160,14 +188,38 @@ public class JwtAuthenticator implements TokenAuthenticator {
      * @since 1.8.2
      */
     public String getSigningSecret() {
-        return signingSecret;
+        return new String(key.getEncoded(), Charset.forName("UTF-8"));
     }
 
     /**
      * @since 1.8.2
      */
     public void setSigningSecret(final String signingSecret) {
-        this.signingSecret = signingSecret;
+    	X509Certificate cert = X509CertUtils.parse(signingSecret);
+    	if (cert == null) {
+    		this.key = new SecretKeySpec(signingSecret.getBytes(Charset.forName("UTF-8")), "AES");
+    	} else {
+    		this.key = cert.getPublicKey();
+    	}
+    }
+    
+    /**
+     * @throws NoSuchAlgorithmException 
+     * @throws InvalidKeySpecException 
+     * @since 1.8.2
+     */
+    public void setSigningPem(final String publicKeyPEM, final String algorithm) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    	// from http://stackoverflow.com/questions/35739932/how-do-i-decode-a-jwt-token-using-an-rsa-public-key-in-pem-format
+    	// decode to its constituent bytes
+    	String publicKeyPEMToUse = publicKeyPEM;
+    	publicKeyPEMToUse = publicKeyPEMToUse.replace("-----BEGIN PUBLIC KEY-----\n", "");
+    	publicKeyPEMToUse = publicKeyPEMToUse.replace("-----END PUBLIC KEY-----", "");
+    	byte[] publicKeyBytes = javax.xml.bind.DatatypeConverter.parseBase64Binary(publicKeyPEMToUse);
+
+    	java.security.spec.X509EncodedKeySpec keySpec = new java.security.spec.X509EncodedKeySpec(publicKeyBytes);
+
+    	KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+    	this.key = keyFactory.generatePublic(keySpec);
     }
 
     /**
@@ -187,7 +239,17 @@ public class JwtAuthenticator implements TokenAuthenticator {
     @Deprecated
     public void setSecret(String secret) {
         this.encryptionSecret = secret;
-        this.signingSecret = secret;
+        setSigningSecret(secret);
         warning();
     }
+
+	public Key getKey() {
+		return key;
+	}
+
+	public void setKey(Key key) {
+		this.key = key;
+	}
+    
+    
 }
