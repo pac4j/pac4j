e9a7f546329a0530be1ce6a19e5d1dce44cf13d2
leleuj <leleuj@gmail.com>
2016-06-03 07:48:07 +0200

    move warning

--- pac4j-jwt/src/main/java/org/pac4j/jwt/profile/JwtGenerator.java
+++ pac4j-jwt/src/main/java/org/pac4j/jwt/profile/JwtGenerator.java
@@ -52,8 +52,8 @@ public class JwtGenerator<U extends CommonProfile> {
         this.signingSecret = secret;
         if (encrypted) {
             this.encryptionSecret = secret;
+            logger.warn("Using the same key for signing and encryption may lead to security vulnerabilities. Consider using different keys");
         }
-        logger.warn("Using the same key for signing and encryption may lead to security vulnerabilities. Consider using different keys");
     }
 
     /**
