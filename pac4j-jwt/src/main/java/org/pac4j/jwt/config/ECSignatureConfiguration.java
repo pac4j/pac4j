0429e8c3f477bf99148c58413e9448e2b471e498
Jérôme LELEU <jerome.leleu@sfr.com>
2016-08-04 14:42:47 +0200

    verify signatures

---
+++ pac4j-jwt/src/main/java/org/pac4j/jwt/config/ECSignatureConfiguration.java
@@ -0,0 +1,133 @@
+package org.pac4j.jwt.config;
+
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.ECDSASigner;
+import com.nimbusds.jose.crypto.ECDSAVerifier;
+import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import org.pac4j.core.exception.TechnicalException;
+import org.pac4j.core.util.CommonHelper;
+import org.pac4j.core.util.InitializableObject;
+
+import java.security.KeyPair;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
+
+/**
+ * Elliptic curve signature configuration: http://connect2id.com/products/nimbus-jose-jwt/examples/jwt-with-ec-signature
+ *
+ * @author Jerome Leleu
+ * @since 1.9.2
+ */
+public class ECSignatureConfiguration extends InitializableObject implements SignatureConfiguration {
+
+    private ECPublicKey publicKey;
+
+    private ECPrivateKey privateKey;
+
+    private JWSAlgorithm algorithm = JWSAlgorithm.ES256;
+
+    public ECSignatureConfiguration() {}
+
+    public ECSignatureConfiguration(final KeyPair keyPair) {
+        setKeyPair(keyPair);
+    }
+
+    public ECSignatureConfiguration(final KeyPair keyPair, final JWSAlgorithm algorithm) {
+        setKeyPair(keyPair);
+        this.algorithm = algorithm;
+    }
+
+    public ECSignatureConfiguration(final ECKey jwk) {
+        setECKey(jwk);
+    }
+
+    @Override
+    protected void internalInit() {
+        CommonHelper.assertNotNull("algorithm", algorithm);
+
+        if (!supports(this.algorithm)) {
+            throw new TechnicalException("Only the ES256, ES384 and ES512 algorithms are supported for elliptic curve signature");
+        }
+    }
+
+
+    @Override
+    public boolean supports(final JWSAlgorithm algorithm) {
+        return algorithm != null && ECDSAVerifier.SUPPORTED_ALGORITHMS.contains(algorithm);
+    }
+
+    @Override
+    public SignedJWT sign(JWTClaimsSet claims) {
+        init();
+        CommonHelper.assertNotNull("privateKey", privateKey);
+
+        try {
+            final JWSSigner signer = new ECDSASigner(this.privateKey);
+            final SignedJWT signedJWT = new SignedJWT(new JWSHeader(algorithm), claims);
+            signedJWT.sign(signer);
+            return signedJWT;
+        } catch (final JOSEException e) {
+            throw new TechnicalException(e);
+        }
+    }
+
+    @Override
+    public boolean verify(final SignedJWT jwt) {
+        init();
+        CommonHelper.assertNotNull("publicKey", publicKey);
+
+        try {
+            final JWSVerifier verifier = new ECDSAVerifier(this.publicKey);
+            return jwt.verify(verifier);
+        } catch (final JOSEException e) {
+            throw new TechnicalException(e);
+        }
+    }
+
+    public void setECKey(final ECKey jwk) {
+        CommonHelper.assertNotNull("jwk", jwk);
+        try {
+            this.privateKey = jwk.toECPrivateKey();
+            this.publicKey = jwk.toECPublicKey();
+        } catch (final JOSEException e) {
+            throw new TechnicalException(e);
+        }
+    }
+
+    public void setKeyPair(final KeyPair keyPair) {
+        CommonHelper.assertNotNull("keyPair", keyPair);
+        this.privateKey = (ECPrivateKey) keyPair.getPrivate();
+        this.publicKey = (ECPublicKey) keyPair.getPublic();
+    }
+
+    public ECPublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(final ECPublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public ECPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(final ECPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public JWSAlgorithm getAlgorithm() {
+        return algorithm;
+    }
+
+    public void setAlgorithm(final JWSAlgorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    @Override
+    public String toString() {
+        return CommonHelper.toString(this.getClass(), "keys", "[protected]", "algorithm", algorithm);
+    }
+}
--- pac4j-jwt/src/main/java/org/pac4j/jwt/credentials/authenticator/JwtAuthenticator.java
+++ pac4j-jwt/src/main/java/org/pac4j/jwt/credentials/authenticator/JwtAuthenticator.java
@@ -1,10 +1,6 @@
 package org.pac4j.jwt.credentials.authenticator;
 
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.JWSVerifier;
-import com.nimbusds.jose.crypto.factories.DefaultJWSVerifierFactory;
-import com.nimbusds.jose.proc.JWSVerifierFactory;
-import com.nimbusds.jose.util.X509CertUtils;
+import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jwt.EncryptedJWT;
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTClaimsSet;
@@ -24,25 +20,18 @@ import org.pac4j.core.credentials.authenticator.TokenAuthenticator;
 import org.pac4j.jwt.JwtConstants;
 import org.pac4j.jwt.config.DirectEncryptionConfiguration;
 import org.pac4j.jwt.config.EncryptionConfiguration;
-import org.pac4j.jwt.config.SigningConfiguration;
+import org.pac4j.jwt.config.MacSignatureConfiguration;
+import org.pac4j.jwt.config.SignatureConfiguration;
 import org.pac4j.jwt.profile.JwtGenerator;
 import org.pac4j.jwt.profile.JwtProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.charset.Charset;
-import java.security.Key;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-import java.security.spec.InvalidKeySpecException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Authenticator for JWT. It creates the user profile and stores it in the credentials
@@ -57,41 +46,33 @@ public class JwtAuthenticator implements TokenAuthenticator {
 
     private EncryptionConfiguration encryptionConfiguration;
 
-    private List<SigningConfiguration> signingConfigurations = new ArrayList<>();
-
-    private JWSVerifierFactory factory = new DefaultJWSVerifierFactory();
-
-	private Key key;
+    private List<SignatureConfiguration> signatureConfigurations = new ArrayList<>();
 
     public JwtAuthenticator() {}
 
-    public JwtAuthenticator(final List<SigningConfiguration> signingConfigurations) {
-        this.signingConfigurations = signingConfigurations;
+    public JwtAuthenticator(final List<SignatureConfiguration> signatureConfigurations) {
+        this.signatureConfigurations = signatureConfigurations;
     }
 
-    public JwtAuthenticator(final List<SigningConfiguration> signingConfigurations, final EncryptionConfiguration encryptionConfiguration) {
-        this.signingConfigurations = signingConfigurations;
+    public JwtAuthenticator(final List<SignatureConfiguration> signatureConfigurations, final EncryptionConfiguration encryptionConfiguration) {
+        this.signatureConfigurations = signatureConfigurations;
         this.encryptionConfiguration = encryptionConfiguration;
     }
 
     @Deprecated
     public JwtAuthenticator(final String signingSecret) {
         this(signingSecret, signingSecret);
-        logger.warn("Using the same key for signing and encryption may lead to security vulnerabilities. Consider using different keys");
+        logger.warn("Using the same key for signature and encryption may lead to security vulnerabilities. Consider using different keys");
     }
 
     @Deprecated
     public JwtAuthenticator(final String signingSecret, final String encryptionSecret) {
-    	setSigningSecret(signingSecret);
+        if (signingSecret != null) {
+            addSignatureConfiguration(new MacSignatureConfiguration(signingSecret));
+        }
         if (encryptionSecret != null) {
             this.encryptionConfiguration = new DirectEncryptionConfiguration(encryptionSecret);
         }
-    }
-
-    @Deprecated
-    public JwtAuthenticator(final String publicKeyPEM, final String algorithm, final String encryptionSecret) throws NoSuchAlgorithmException, InvalidKeySpecException {
-    	setSigningPem(publicKeyPEM, algorithm);
-        this.encryptionConfiguration = new DirectEncryptionConfiguration(encryptionSecret);
     }
 
     /**
@@ -140,20 +121,26 @@ public class JwtAuthenticator implements TokenAuthenticator {
                 	throw new TechnicalException("unsupported unsecured jwt");
             	}
 
-                CommonHelper.assertNotNull("signingConfigurations", signingConfigurations);
-
-            	CommonHelper.assertNotNull("key", key);
-    			JWSVerifier verifier = factory.createJWSVerifier(signedJWT.getHeader(), key);
-            	verified = signedJWT.verify(verifier);
+                boolean found = false;
+                final JWSAlgorithm jwtAlgorithm = signedJWT.getHeader().getAlgorithm();
+                for (final SignatureConfiguration config : signatureConfigurations) {
+                    if (config.supports(jwtAlgorithm)) {
+                        verified = config.verify(signedJWT);
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    throw new CredentialsException("No signature algorithm found for JWT: " + token);
+                }
             }
         	if (!verified) {
-            	final String message = "JWT verification failed: " + token;
-            	throw new CredentialsException(message);
+            	throw new CredentialsException("JWT verification failed: " + token);
         	}
 
           	createJwtProfile(credentials, jwt);
 
-        } catch (final ParseException | JOSEException e) {
+        } catch (final ParseException e) {
             throw new TechnicalException("Cannot decrypt / verify JWT", e);
         }
     }
@@ -185,49 +172,23 @@ public class JwtAuthenticator implements TokenAuthenticator {
         credentials.setUserProfile(profile);
     }
 
-    public String getSigningSecret() {
-        return new String(key.getEncoded(), Charset.forName("UTF-8"));
+    public List<SignatureConfiguration> getSignatureConfigurations() {
+        return signatureConfigurations;
     }
 
-    public void setSigningSecret(final String signingSecret) {
-    	X509Certificate cert = X509CertUtils.parse(signingSecret);
-    	if (cert == null) {
-    		this.key = new SecretKeySpec(signingSecret.getBytes(Charset.forName("UTF-8")), "AES");
-    	} else {
-    		this.key = cert.getPublicKey();
-    	}
-    }
-    
-    /**
-     * Define the signing certificate.
-     *
-     * @param publicKeyPEM the public key certificate
-     * @param algorithm the key algorithm
-     * @throws NoSuchAlgorithmException No such algorithm exception
-     * @throws InvalidKeySpecException  Invalid key exception
-     * @since 1.8.2
-     */
-    public void setSigningPem(final String publicKeyPEM, final String algorithm) throws NoSuchAlgorithmException, InvalidKeySpecException {
-    	// from http://stackoverflow.com/questions/35739932/how-do-i-decode-a-jwt-token-using-an-rsa-public-key-in-pem-format
-    	// decode to its constituent bytes
-    	String publicKeyPEMToUse = publicKeyPEM;
-    	publicKeyPEMToUse = publicKeyPEMToUse.replaceAll("-----BEGIN PUBLIC KEY-----[\\r\\n]+", "");
-    	publicKeyPEMToUse = publicKeyPEMToUse.replace("-----END PUBLIC KEY-----", "");
-    	byte[] publicKeyBytes = javax.xml.bind.DatatypeConverter.parseBase64Binary(publicKeyPEMToUse);
-
-    	java.security.spec.X509EncodedKeySpec keySpec = new java.security.spec.X509EncodedKeySpec(publicKeyBytes);
-
-    	KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
-    	this.key = keyFactory.generatePublic(keySpec);
+    public void setSignatureConfiguration(final SignatureConfiguration signatureConfiguration) {
+        addSignatureConfiguration(signatureConfiguration);
     }
 
-	public Key getKey() {
-		return key;
-	}
+    public void addSignatureConfiguration(final SignatureConfiguration signatureConfiguration) {
+        CommonHelper.assertNotNull("signatureConfiguration", signatureConfiguration);
+        signatureConfigurations.add(signatureConfiguration);
+    }
 
-	public void setKey(Key key) {
-		this.key = key;
-	}
+    public void setSignatureConfigurations(final List<SignatureConfiguration> signatureConfigurations) {
+        CommonHelper.assertNotNull("signatureConfigurations", signatureConfigurations);
+        this.signatureConfigurations = signatureConfigurations;
+    }
 
     public EncryptionConfiguration getEncryptionConfiguration() {
         return encryptionConfiguration;
@@ -235,5 +196,10 @@ public class JwtAuthenticator implements TokenAuthenticator {
 
     public void setEncryptionConfiguration(final EncryptionConfiguration encryptionConfiguration) {
         this.encryptionConfiguration = encryptionConfiguration;
+    }
+
+    @Override
+    public String toString() {
+        return CommonHelper.toString(this.getClass(), "signatureConfigurations", signatureConfigurations, "encryptionConfiguration", encryptionConfiguration);
     }
 }
