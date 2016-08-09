90ee2ac0bee796878b4aa9abf2d46b698b813f09
zhangzhenli <zhangzhenli@live.com>
2016-08-09 12:34:25 +0800

    improve error message

--- pac4j-core/src/test/java/org/pac4j/core/credentials/extractor/HeaderExtractorTests.java
+++ pac4j-core/src/test/java/org/pac4j/core/credentials/extractor/HeaderExtractorTests.java
@@ -2,13 +2,14 @@ package org.pac4j.core.credentials.extractor;
 
 import org.junit.Test;
 import org.pac4j.core.context.MockWebContext;
+import org.pac4j.core.credentials.TokenCredentials;
 import org.pac4j.core.exception.CredentialsException;
 import org.pac4j.core.exception.HttpAction;
 import org.pac4j.core.util.TestsConstants;
-import org.pac4j.core.credentials.TokenCredentials;
 import org.pac4j.core.util.TestsHelper;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * This class tests the {@link HeaderExtractor}.
@@ -43,6 +44,7 @@ public final class HeaderExtractorTests implements TestsConstants {
     @Test
     public void testBadPrefix() throws HttpAction {
         final MockWebContext context = MockWebContext.create().addRequestHeader(GOOD_HEADER, BAD_PREFIX + VALUE);
-        TestsHelper.expectException(() -> extractor.extract(context), CredentialsException.class, "Wrong prefix for header: " + GOOD_HEADER);
+        TestsHelper.expectException(() -> extractor.extract(context), CredentialsException.class, "Wrong prefix for header: \"" + GOOD_HEADER + ": " + BAD_PREFIX + VALUE
+                + "\",Expected matching RegExp : \"" + this.GOOD_HEADER + ": " + GOOD_PREFIX + ".*\"");
     }
 }
