a93e13e6d39b5b95e0d45c869c407c35ab0cf4d7
leleuj <leleuj@gmail.com>
2016-09-22 18:35:12 +0200

    Upgrade Dropbox support

---
+++ pac4j-oauth/src/main/java/org/pac4j/scribe/builder/api/DropboxApi20.java
@@ -0,0 +1,35 @@
+package org.pac4j.scribe.builder.api;
+
+import com.github.scribejava.core.builder.api.DefaultApi20;
+import com.github.scribejava.core.model.OAuthConfig;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.utils.OAuthEncoder;
+
+/**
+ * This class represents the OAuth API implementation for DropBox using OAuth protocol version 2.
+ *
+ * @author Jerome Leleu
+ * @since 1.9.3
+ */
+public class DropboxApi20 extends DefaultApi20 {
+
+    public final static DropboxApi20 INSTANCE  = new DropboxApi20();
+
+    private static final String AUTH_URL = "https://www.dropbox.com/1/oauth2/authorize";
+    private static final String TOKEN_URL = "https://www.dropbox.com/1/oauth2/token";
+
+    @Override
+    public String getAccessTokenEndpoint() {
+        return TOKEN_URL;
+    }
+
+    @Override
+    public String getAuthorizationUrl(OAuthConfig oAuthConfig) {
+        return String.format(AUTH_URL + "?client_id=%s&response_type=code&redirect_uri=%s", oAuthConfig.getApiKey(), OAuthEncoder.encode(oAuthConfig.getCallback()));
+    }
+
+    @Override
+    public Verb getAccessTokenVerb() {
+        return Verb.POST;
+    }
+}
