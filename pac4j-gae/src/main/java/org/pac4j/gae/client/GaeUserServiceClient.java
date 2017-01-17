a0c82ac6f1e1b2bcd2ad9527dfe1e3f4a10c077f
leleuj <leleuj@gmail.com>
2017-01-17 06:08:15 +0100

    GAE

--- pac4j-gae/src/main/java/org/pac4j/gae/client/GaeUserServiceClient.java
+++ pac4j-gae/src/main/java/org/pac4j/gae/client/GaeUserServiceClient.java
@@ -1,9 +1,8 @@
 package org.pac4j.gae.client;
 
-import org.pac4j.core.client.IndirectClient;
+import org.pac4j.core.client.IndirectClientV2;
 import org.pac4j.core.redirect.RedirectAction;
 import org.pac4j.core.context.WebContext;
-import org.pac4j.core.exception.HttpAction;
 import org.pac4j.core.profile.definition.CommonProfileDefinition;
 import org.pac4j.core.util.CommonHelper;
 import org.pac4j.gae.credentials.GaeUserCredentials;
@@ -19,48 +18,41 @@ import com.google.appengine.api.users.UserServiceFactory;
  * @author Patrice de Saint Steban
  * @since 1.6.0
  */
-public class GaeUserServiceClient extends IndirectClient<GaeUserCredentials, GaeUserServiceProfile> {
+public class GaeUserServiceClient extends IndirectClientV2<GaeUserCredentials, GaeUserServiceProfile> {
 
 	protected UserService service;
 	protected String authDomain = null;
 
 	@Override
-	protected void internalInit(final WebContext context) {
+    protected void internalInit(final WebContext context) {
+	    super.internalInit(context);
+
 		service = UserServiceFactory.getUserService();
 		CommonHelper.assertNotNull("service", this.service);
-		CommonHelper.assertNotBlank("callbackUrl", this.callbackUrl);
 		setProfileDefinition(new CommonProfileDefinition<>(x -> new GaeUserServiceProfile()));
-	}
-
-	@Override
-	protected RedirectAction retrieveRedirectAction(WebContext context) {
-		String destinationUrl = computeFinalCallbackUrl(context);
-		String loginUrl = authDomain == null ?  service.createLoginURL(destinationUrl) : service.createLoginURL(destinationUrl, authDomain);
-		return RedirectAction.redirect(loginUrl);
-	}
-
-	@Override
-	protected GaeUserCredentials retrieveCredentials(WebContext context)
-			throws HttpAction {
-		GaeUserCredentials credentials = new GaeUserCredentials();
-		credentials.setUser(service.getCurrentUser());
-		return credentials;
-	}
-
-	@Override
-	protected GaeUserServiceProfile retrieveUserProfile(GaeUserCredentials credentials, WebContext context) throws HttpAction {
-		User user = credentials.getUser();
-		if (user != null) {
-			final GaeUserServiceProfile profile = getProfileDefinition().newProfile();
-			profile.setId(user.getEmail());
-			getProfileDefinition().convertAndAdd(profile, CommonProfileDefinition.EMAIL, user.getEmail());
-			getProfileDefinition().convertAndAdd(profile, CommonProfileDefinition.DISPLAY_NAME, user.getNickname());
-			if (service.isUserAdmin()) {
-				profile.addRole(GaeUserServiceProfile.PAC4J_GAE_GLOBAL_ADMIN_ROLE);
+		setRedirectActionBuilder(ctx -> {
+			String destinationUrl = computeFinalCallbackUrl(ctx);
+			String loginUrl = authDomain == null ?  service.createLoginURL(destinationUrl) : service.createLoginURL(destinationUrl, authDomain);
+			return RedirectAction.redirect(loginUrl);
+		});
+		setCredentialsExtractor(ctx -> {
+			GaeUserCredentials credentials = new GaeUserCredentials();
+			credentials.setUser(service.getCurrentUser());
+			return credentials;
+		});
+		setAuthenticator((credentials, ctx) -> {
+			User user = credentials.getUser();
+			if (user != null) {
+				final GaeUserServiceProfile profile = getProfileDefinition().newProfile();
+				profile.setId(user.getEmail());
+				getProfileDefinition().convertAndAdd(profile, CommonProfileDefinition.EMAIL, user.getEmail());
+				getProfileDefinition().convertAndAdd(profile, CommonProfileDefinition.DISPLAY_NAME, user.getNickname());
+				if (service.isUserAdmin()) {
+					profile.addRole(GaeUserServiceProfile.PAC4J_GAE_GLOBAL_ADMIN_ROLE);
+				}
+				credentials.setUserProfile(profile);
 			}
-			return profile;
-		}
-		return null;
+		});
 	}
 
 	/**
