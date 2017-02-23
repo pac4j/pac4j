99caf563ad8a1bf92b16e70f074d5c48ce98d26a
LELEU Jérôme <leleuj@gmail.com>
2017-02-23 16:05:43 +0100

    Remove default implem from the SessionStore and the getSessionStore (#834)

--- pac4j-core/src/main/java/org/pac4j/core/context/session/SessionStore.java
+++ pac4j-core/src/main/java/org/pac4j/core/context/session/SessionStore.java
@@ -42,33 +42,24 @@ public interface SessionStore<C extends WebContext> {
      * @param context the web context
      * @return whether the session has been destroyed
      */
-    default boolean destroySession(C context) {
-        // by default, the session has not been destroyed
-        return false;
-    }
+    boolean destroySession(C context);
 
     /**
      * Get the native session as a trackable object.
      *
      * @param context the web context
-     * @return the trackable object
+     * @return the trackable object or <code>null</code> if this is not supported
      */
-    default Object getTrackableSession(C context) {
-        // by default, the session store does not know how to keep track the native session
-        return null;
-    }
+    Object getTrackableSession(C context);
 
     /**
      * Build a new session store from a trackable session.
      *
      * @param context the web context
      * @param trackableSession the trackable session
-     * @return the new session store
+     * @return the new session store or <code>null</code> if this is not supported
      */
-    default SessionStore<C> buildFromTrackableSession(C context, Object trackableSession) {
-        // by default, the session store does not know how to build a new session store
-        return null;
-    }
+    SessionStore<C> buildFromTrackableSession(C context, Object trackableSession);
 
     /**
      * Renew the native session by copying all data to a new one.
@@ -76,8 +67,5 @@ public interface SessionStore<C extends WebContext> {
      * @param context the web context
      * @return whether the session store has renewed the session
      */
-    default boolean renewSession(C context) {
-        // by default, the session store does not know how to renew the native session
-        return false;
-    }
+    boolean renewSession(C context);
 }
