f6b95bc30ad6b326325a6285ba48cbc2d36763cc
Elie Roux <elie.roux@telecom-bretagne.eu>
2017-05-15 15:07:35 +0200

    Add CouchProfileService (#906)
    * CouchDB: first working code, not all tests pass
    
    * all tests pass
    
    * add doc
    
    * cleanup
    
    * amend code with PR review comments
    
    * make objectMapper private
    
    * address more PR comments
    
    * use default couchdb port for tests
    
    * use mcouch
    
    * use 1.0.0 version of mcouch
    
    * fix bugs found by findbugs
    
    * fix javadoc

---
+++ pac4j-couch/src/test/java/org/pac4j/couch/test/tools/CouchServer.java
@@ -0,0 +1,53 @@
+package org.pac4j.couch.test.tools;
+
+import org.apache.shiro.authc.credential.DefaultPasswordService;
+import org.ektorp.CouchDbConnector;
+import org.ektorp.http.StdHttpClient;
+import org.ektorp.impl.StdCouchDbConnector;
+import org.ektorp.impl.StdCouchDbInstance;
+import org.pac4j.core.credentials.password.ShiroPasswordEncoder;
+import org.pac4j.core.util.TestsConstants;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import mcouch.core.InMemoryCouchDb;
+
+import org.pac4j.core.credentials.password.PasswordEncoder;
+
+import java.io.IOException;
+
+/**
+ * Uses a local CouchDB server.
+ *
+ * @author Elie Roux
+ * @since 2.0.0
+ */
+public final class CouchServer implements TestsConstants {
+
+	public final static PasswordEncoder PASSWORD_ENCODER = new ShiroPasswordEncoder(new DefaultPasswordService());
+
+	public CouchDbConnector start() {
+		InMemoryCouchDb couchDbClient = new InMemoryCouchDb();
+		couchDbClient.createDatabase("users");
+		StdHttpClient stdHttpClient = new StdHttpClient(couchDbClient);
+		StdCouchDbInstance stdCouchDbInstance = new StdCouchDbInstance(stdHttpClient);
+		StdCouchDbConnector couchDbConnector    = new StdCouchDbConnector("users", stdCouchDbInstance);
+
+		couchDbConnector.createDatabaseIfNotExists();
+
+		// uploading design doc:
+		String designDocString = "{\"_id\":\"_design/pac4j\",\"language\":\"javascript\",\"views\":{\"by_username\":{\"map\":\"function(doc){if (doc.username) emit(doc.username, doc);}\"},\"by_linkedid\":{\"map\":\"function(doc) {if (doc.linkedid) emit(doc.linkedid, doc);}\"}}}";
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			couchDbConnector.create(objectMapper.readTree(designDocString));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return couchDbConnector;
+	}
+
+	public void stop() {
+	}
+}
