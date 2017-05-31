e5f54cebdf60e3695651895a49428288dc655ab9
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2017-05-31 10:39:29 -0300

    Merge IpHeaderChainExtractor into IpExtractor and add proxyIp
    - Add all functionalities from IpHeaderChainExtractor to IpExtractor
    - Deprecate get/set alternateAlternateIpHeader in favor of get/set AlternateIpHeaders
    - Update get/set alternateAlternateIpHeader to work with the new internal implementation for backward compatibility
    - Add proxyIp:
        + verified if the remotehost if proxyIp in case of header search
        + defaults to ""
        + this can checks if the request originated from the right proxy server

--- pac4j-http/src/main/java/org/pac4j/http/credentials/extractor/IpExtractor.java
+++ pac4j-http/src/main/java/org/pac4j/http/credentials/extractor/IpExtractor.java
@@ -6,28 +6,45 @@ import org.pac4j.core.credentials.extractor.CredentialsExtractor;
 import org.pac4j.core.exception.HttpAction;
 import org.pac4j.core.util.CommonHelper;
 
+import java.util.*;
+
 /**
  * To extract a remote IP address.
+ * Search for headers as defined in an array.
+ * The first match will be returned as specified for {@code enhanced for} iteration over arrays.
+ * By default, if no proxy ip is setted ({@link #setProxyIp(String)}), only request from proxy IP will be accepted.
  *
  * @author Jerome Leleu
+ * @author Guilherme I F L Weizenmann
  * @since 1.8.0
  */
 public class IpExtractor implements CredentialsExtractor<TokenCredentials> {
 
     private final String clientName;
 
-    private String alternateIpHeader;
+    private List<String> alternateIpHeaders = Collections.emptyList();
+
+    private String proxyIp = "127.0.0.1";
 
     public IpExtractor(final String clientName) {
         this.clientName = clientName;
     }
 
+    public IpExtractor(final String clientName, String... alternateIpHeaders) {
+        this.clientName = clientName;
+        this.alternateIpHeaders = Arrays.asList(alternateIpHeaders);
+    }
+
     public TokenCredentials extract(WebContext context) throws HttpAction {
-        final String ip;
-        if (alternateIpHeader == null) {
+        String ip = null;
+        if (alternateIpHeaders.isEmpty()) {
             ip = context.getRemoteAddr();
         } else {
-            ip = context.getRequestHeader(alternateIpHeader);
+            String requestSourceIp = context.getRemoteAddr();
+            // if using proxy, check if the ip proxy is correct
+            if (!this.proxyIp.isEmpty() && this.proxyIp.equals(requestSourceIp)) {
+                ip = ipFromHeaders(context);
+            }
         }
 
         if (ip == null) {
@@ -37,16 +54,77 @@ public class IpExtractor implements CredentialsExtractor<TokenCredentials> {
         return new TokenCredentials(ip, clientName);
     }
 
-    public String getAlternateIpHeader() {
-        return alternateIpHeader;
+    private String ipFromHeaders(WebContext context) {
+        String ip;
+        for (String header : alternateIpHeaders) {
+            ip = context.getRequestHeader(header);
+            if (ip != null && !ip.isEmpty()) {
+                return ip;
+            }
+        }
+        return null;
     }
 
+    /**
+     * @return The alternate header to search for IP, or null if absent.
+     * @deprecated Use {@link #getAlternateIpHeaders()} instead.
+     */
+    @Deprecated
+    public String getAlternateIpHeader() {
+        return this.alternateIpHeaders.isEmpty() ? null : alternateIpHeaders.get(0);
+    }
+
+    /**
+     * @param alternateIpHeader The alternate header to search for IP.
+     * @deprecated Use {@link #setAlternateIpHeaders(String...)} instead.
+     */
+    @Deprecated
     public void setAlternateIpHeader(final String alternateIpHeader) {
-        this.alternateIpHeader = alternateIpHeader;
+        if (alternateIpHeader == null) {
+            this.alternateIpHeaders = Collections.emptyList();
+        } else {
+            this.alternateIpHeaders = new LinkedList<String>() {{
+                add(alternateIpHeader);
+            }};
+        }
+    }
+
+    /**
+     * @return The verified proxy IP
+     * @since 2.1.0
+     */
+    public String getProxyIp() {
+        return proxyIp;
+    }
+
+    /**
+     * @param proxyIp Set the IP to verify the proxy request source
+     * @since 2.1.0
+     */
+    public void setProxyIp(String proxyIp) {
+        this.proxyIp = proxyIp;
+    }
+
+    /**
+     * @return Defined headers to search for IP as {@link Collections#unmodifiableList(List)}
+     * @since 2.1.0
+     */
+    public List getAlternateIpHeaders() {
+        return Collections.unmodifiableList(this.alternateIpHeaders);
+    }
+
+    /**
+     * @param alternateIpHeaders Sets alternate headers to search for IP.
+     *                           The first match will be returned as specified for {@code enhanced for} iteration over arrays.
+     * @since 2.1.0
+     */
+    public void setAlternateIpHeaders(final String... alternateIpHeaders) {
+        Objects.requireNonNull(alternateIpHeaders, "Ip headers must be not null");
+        this.alternateIpHeaders = Arrays.asList(alternateIpHeaders);
     }
 
     @Override
     public String toString() {
-        return CommonHelper.toString(this.getClass(), "clientName", this.clientName, "alternateIpHeader", this.alternateIpHeader);
+        return CommonHelper.toString(this.getClass(), "clientName", this.clientName, "alternateIpHeaders", Arrays.asList(this.alternateIpHeaders));
     }
 }
--- pac4j-http/src/test/java/org/pac4j/http/credentials/extractor/IpExtractorTests.java
+++ pac4j-http/src/test/java/org/pac4j/http/credentials/extractor/IpExtractorTests.java
@@ -2,23 +2,25 @@ package org.pac4j.http.credentials.extractor;
 
 import org.junit.Test;
 import org.pac4j.core.context.MockWebContext;
+import org.pac4j.core.credentials.TokenCredentials;
 import org.pac4j.core.exception.HttpAction;
 import org.pac4j.core.util.TestsConstants;
-import org.pac4j.core.credentials.TokenCredentials;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * This class tests the {@link IpExtractor}.
- * 
+ *
  * @author Jerome Leleu
  * @since 1.8.0
  */
 public final class IpExtractorTests implements TestsConstants {
 
     private final static String GOOD_IP = "goodIp";
+    private final static String LOCALHOST = "127.0.0.1";
 
-    private final static IpExtractor extractor = new IpExtractor(CLIENT_NAME);
+    private static final IpExtractor extractor = new IpExtractor(CLIENT_NAME);
 
     @Test
     public void testRetrieveIpOk() throws HttpAction {
@@ -28,12 +30,46 @@ public final class IpExtractorTests implements TestsConstants {
     }
 
     @Test
-    public void testRetrieveIpFromHeader() throws HttpAction {
-        final MockWebContext context = MockWebContext.create().addRequestHeader(HEADER_NAME, GOOD_IP);
+    @Deprecated
+    public void testRetrieveIpFromHeaderDeprecated() throws HttpAction {
+        final MockWebContext context = MockWebContext.create().addRequestHeader(HEADER_NAME, GOOD_IP).setRemoteAddress(LOCALHOST);
         final IpExtractor ipExtractor = new IpExtractor(CLIENT_NAME);
         ipExtractor.setAlternateIpHeader(HEADER_NAME);
         final TokenCredentials credentials = ipExtractor.extract(context);
         assertEquals(GOOD_IP, credentials.getToken());
+    }
+
+    @Test
+    public void testRetrieveIpFromHeader() throws HttpAction {
+        final MockWebContext context = MockWebContext.create().addRequestHeader(HEADER_NAME, GOOD_IP).setRemoteAddress(LOCALHOST);
+        final IpExtractor ipExtractor = new IpExtractor(CLIENT_NAME);
+        // test for varargs
+        ipExtractor.setAlternateIpHeaders("fooBar", HEADER_NAME, "barFoo");
+        final TokenCredentials credentials = ipExtractor.extract(context);
+        assertEquals(GOOD_IP, credentials.getToken());
+        // test for edge case of 1 header
+        ipExtractor.setAlternateIpHeaders(HEADER_NAME);
+        final TokenCredentials credentials2 = ipExtractor.extract(context);
+        assertEquals(GOOD_IP, credentials2.getToken());
+    }
+
+    @Test
+    public void testRetrieveIpFromHeaderUsingConstructor() throws HttpAction {
+        final MockWebContext context = MockWebContext.create().addRequestHeader(HEADER_NAME, GOOD_IP).setRemoteAddress(LOCALHOST);
+        // test for varargs
+        final IpExtractor ipExtractor = new IpExtractor(CLIENT_NAME, "fooBar", HEADER_NAME, "barFoo");
+        final TokenCredentials credentials = ipExtractor.extract(context);
+        assertEquals(GOOD_IP, credentials.getToken());
+        // test for edge case of 1 header
+        final IpExtractor ipExtractor2 = new IpExtractor(CLIENT_NAME, HEADER_NAME);
+        final TokenCredentials credentials2 = ipExtractor2.extract(context);
+        assertEquals(GOOD_IP, credentials2.getToken());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSetNullIpHeaderChain() throws HttpAction {
+        final IpExtractor ipExtractor = new IpExtractor(CLIENT_NAME);
+        ipExtractor.setAlternateIpHeaders(null);
     }
 
     @Test
@@ -42,4 +78,12 @@ public final class IpExtractorTests implements TestsConstants {
         final TokenCredentials credentials = extractor.extract(context);
         assertNull(credentials);
     }
+
+    @Test
+    public void testWrongProxyIp() throws HttpAction {
+        final MockWebContext context = MockWebContext.create();
+        final TokenCredentials credentials = extractor.extract(context);
+        assertNull(credentials);
+    }
+
 }
