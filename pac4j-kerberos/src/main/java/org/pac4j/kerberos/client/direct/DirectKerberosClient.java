1ac49faa75212d56398113f5950a17e88b235ff8
vidma <vidmantas.zemleris@gmail.com>
2017-05-31 09:19:45 +0300

    Add improved pac4j-kerberos (#919)
    * Add Original Kerberos implementation
    
    * Reformat pac4j-kerberos code
    
    * Improve Kerberos implementation
    
    - Rebase over master, fixing changes to APIs
    - Use regular HttpAction instead of a custom DeferredHttpAction
    - Fix bug when absolute keytab path was lost (using only filename)
    
    * Add integration/unit test with Kerby
    
    * Minor fixes
    
    * Rename KerberosClient -> DirectKerberosClient
    
    * Refactor init() usage
    
    * Add a dummy but working IndirectKerberosClient
    
    * Move exception handling out of extractor to IndirectClient
    
    * Add more tests for direct client
    
    * Add tests for both direct and indirect clients
    
    * Fix imports
    
    * Add docs for pac4j-kerberos
    
    * Fix BeforeAll/AfterAll in Kerby test

---
+++ pac4j-kerberos/src/main/java/org/pac4j/kerberos/client/direct/DirectKerberosClient.java
@@ -0,0 +1,37 @@
+package org.pac4j.kerberos.client.direct;
+
+
+import org.pac4j.core.client.DirectClient;
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.credentials.authenticator.Authenticator;
+import org.pac4j.core.profile.creator.ProfileCreator;
+import org.pac4j.kerberos.credentials.KerberosCredentials;
+import org.pac4j.kerberos.credentials.extractor.KerberosExtractor;
+import org.pac4j.kerberos.profile.KerberosProfile;
+
+/**
+ * <p>This class is the client to authenticate users directly based on Kerberos ticket.
+ * (i.e. being a direct client it do not redirects to callback URLs).</p>
+ *
+ * @author Garry Boyce
+ * @since 2.1.0
+ */
+public class DirectKerberosClient extends DirectClient<KerberosCredentials, KerberosProfile> {
+
+    public DirectKerberosClient() {
+    }
+
+    public DirectKerberosClient(final Authenticator authenticator) {
+        setAuthenticator(authenticator);
+    }
+
+    public DirectKerberosClient(final Authenticator authenticator, final ProfileCreator<KerberosCredentials, KerberosProfile> profileCreator) {
+        setAuthenticator(authenticator);
+        setProfileCreator(profileCreator);
+    }
+
+    @Override
+    protected void clientInit(final WebContext context) {
+        defaultCredentialsExtractor(new KerberosExtractor(getName()));
+    }
+}
---
+++ pac4j-kerberos/src/main/java/org/pac4j/kerberos/credentials/extractor/KerberosExtractor.java
@@ -0,0 +1,46 @@
+package org.pac4j.kerberos.credentials.extractor;
+
+import org.pac4j.core.context.HttpConstants;
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.credentials.extractor.CredentialsExtractor;
+import org.pac4j.core.exception.CredentialsException;
+import org.pac4j.core.exception.HttpAction;
+import org.pac4j.kerberos.credentials.KerberosCredentials;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+/**
+ * To extract Kerberos headers.
+ *
+ * @author Garry Boyce
+ * @since 2.1.0
+ */
+public class KerberosExtractor implements CredentialsExtractor<KerberosCredentials> {
+    private final String clientName;
+
+    public KerberosExtractor(final String clientName) {
+        this.clientName = clientName;
+    }
+
+    @Override
+    public KerberosCredentials extract(WebContext context) throws CredentialsException, HttpAction {
+        final String header = context.getRequestHeader(HttpConstants.AUTHORIZATION_HEADER);
+        if (header == null) {
+            return null;
+        }
+
+        if (!(header.startsWith("Negotiate ") || header.startsWith("Kerberos "))) {
+            // "Authorization" header do not indicate Kerberos mechanism yet,
+            // so the extractor shouldn't throw an exception
+            return null;
+        }
+
+        byte[] base64Token = header.substring(header.indexOf(" ") + 1).getBytes(StandardCharsets.UTF_8);
+        byte[] kerberosTicket = Base64.getDecoder().decode(base64Token);
+
+        return new KerberosCredentials(kerberosTicket, clientName);
+
+    }
+
+}
