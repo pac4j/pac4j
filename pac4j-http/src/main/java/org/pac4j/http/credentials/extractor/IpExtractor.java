44ef85dd58756cbab1b948a06883a76e9d98a8a8
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2017-06-02 15:42:30 -0300

    Add ip extractor for multiple headers in chain (#924)
    * Add ip extractor for multiple headers in chain
    
    - Add IpHeaderChainExtractor to search for ip header
    - Add tests
    
    * Fix javadocs on IpHeaderChainExtractor
    
    * Update javadocs on IpHeaderChainExtractor
    
    * Fix findbug error
    
    - getAlternateIpHeaders() may expose internal representation by returning IpHeaderChainExtractor.alternateIpHeaders
    
    * Update docs, fix typo in IpHeaderChainExtractor
    
    - update docs about ip extrator to have an example about IpHeaderChainExtractor
    - fix typo in IpHeaderChainExtractor.setAlternateIpHeaders
    
    * Merge IpHeaderChainExtractor into IpExtractor and add proxyIp
    
    - Add all functionalities from IpHeaderChainExtractor to IpExtractor
    - Deprecate get/set alternateAlternateIpHeader in favor of get/set AlternateIpHeaders
    - Update get/set alternateAlternateIpHeader to work with the new internal implementation for backward compatibility
    - Add proxyIp:
        + verified if the remotehost if proxyIp in case of header search
        + defaults to ""
        + this can checks if the request originated from the right proxy server
    
    * Fix proxyIp check and PMD warning
    
    * Use CommonHelper.assertNotNull for consistency with other checks
    
    * Update docs and javadocs for IpExtractor
    
    * Fix IpExtractor test (expected exception)

--- pac4j-http/src/main/java/org/pac4j/http/credentials/extractor/IpExtractor.java
+++ pac4j-http/src/main/java/org/pac4j/http/credentials/extractor/IpExtractor.java
@@ -6,28 +6,53 @@ import org.pac4j.core.credentials.extractor.CredentialsExtractor;
 import org.pac4j.core.exception.HttpAction;
 import org.pac4j.core.util.CommonHelper;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * To extract a remote IP address.
+ * Search for headers as defined in an array.
+ * The first match will be returned as specified for {@code enhanced for} iteration over arrays.
+ * By default, if no proxy ip is setted ({@link #setProxyIp(String)}), only request from proxy IP will be accepted.
  *
  * @author Jerome Leleu
+ * @author Guilherme I F L Weizenmann
  * @since 1.8.0
  */
 public class IpExtractor implements CredentialsExtractor<TokenCredentials> {
 
     private final String clientName;
 
-    private String alternateIpHeader;
+    private List<String> alternateIpHeaders = Collections.emptyList();
+
+    private String proxyIp = "";
 
     public IpExtractor(final String clientName) {
         this.clientName = clientName;
     }
 
+    public IpExtractor(final String clientName, String... alternateIpHeaders) {
+        this.clientName = clientName;
+        this.alternateIpHeaders = Arrays.asList(alternateIpHeaders);
+    }
+
     public TokenCredentials extract(WebContext context) throws HttpAction {
         final String ip;
-        if (alternateIpHeader == null) {
+        if (alternateIpHeaders.isEmpty()) {
             ip = context.getRemoteAddr();
         } else {
-            ip = context.getRequestHeader(alternateIpHeader);
+            String requestSourceIp = context.getRemoteAddr();
+            if (this.proxyIp.isEmpty()) {
+                ip = ipFromHeaders(context);
+            }
+            // if using proxy, check if the ip proxy is correct
+            else if (this.proxyIp.equals(requestSourceIp)) {
+                ip = ipFromHeaders(context);
+            } else {
+                ip = null;
+            }
         }
 
         if (ip == null) {
@@ -37,16 +62,78 @@ public class IpExtractor implements CredentialsExtractor<TokenCredentials> {
         return new TokenCredentials(ip, clientName);
     }
 
-    public String getAlternateIpHeader() {
-        return alternateIpHeader;
+    private String ipFromHeaders(WebContext context) {
+        String ip;
+        for (String header : alternateIpHeaders) {
+            ip = context.getRequestHeader(header);
+            if (ip != null && !ip.isEmpty()) {
+                return ip;
+            }
+        }
+        return null;
     }
 
+    /**
+     * @return The alternate header to search for IP, or null if absent.
+     * @deprecated Use {@link #getAlternateIpHeaders()} instead.
+     */
+    @Deprecated
+    public String getAlternateIpHeader() {
+        return this.alternateIpHeaders.isEmpty() ? null : alternateIpHeaders.get(0);
+    }
+
+    /**
+     * @param alternateIpHeader The alternate header to search for IP.
+     * @deprecated Use {@link #setAlternateIpHeaders(String...)} instead.
+     */
+    @Deprecated
     public void setAlternateIpHeader(final String alternateIpHeader) {
-        this.alternateIpHeader = alternateIpHeader;
+        if (alternateIpHeader == null) {
+            this.alternateIpHeaders = Collections.emptyList();
+        } else {
+            this.alternateIpHeaders = new LinkedList<String>() {{
+                add(alternateIpHeader);
+            }};
+        }
+    }
+
+    /**
+     * @return The verified proxy IP
+     * @since 2.1.0
+     */
+    public String getProxyIp() {
+        return proxyIp;
+    }
+
+    /**
+     * @param proxyIp Set the IP to verify the proxy request source.
+     *               Setting {@code null} or {@code ""} (empty string) disabled the proxy IP check.
+     * @since 2.1.0
+     */
+    public void setProxyIp(String proxyIp) {
+        this.proxyIp = proxyIp == null ? "" : proxyIp;
+    }
+
+    /**
+     * @return Defined headers to search for IP as {@link Collections#unmodifiableList(List)}
+     * @since 2.1.0
+     */
+    public List getAlternateIpHeaders() {
+        return Collections.unmodifiableList(this.alternateIpHeaders);
+    }
+
+    /**
+     * @param alternateIpHeaders Sets alternate headers to search for IP.
+     *                           The first match will be returned as specified for {@code enhanced for} iteration over arrays.
+     * @since 2.1.0
+     */
+    public void setAlternateIpHeaders(final String... alternateIpHeaders) {
+        CommonHelper.assertNotNull("alternateIpHeaders", alternateIpHeaders);
+        this.alternateIpHeaders = Arrays.asList(alternateIpHeaders);
     }
 
     @Override
     public String toString() {
-        return CommonHelper.toString(this.getClass(), "clientName", this.clientName, "alternateIpHeader", this.alternateIpHeader);
+        return CommonHelper.toString(this.getClass(), "clientName", this.clientName, "alternateIpHeaders", Arrays.asList(this.alternateIpHeaders));
     }
 }
