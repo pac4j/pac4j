0fa8ced40fe55dfe8f0a39eef78b09ef2a300396
Ghenadii Batalski <ghenadii.batalski@gmail.com>
2017-06-24 12:27:32 +0200

    clientName removed from calbackUrl because ignored by Azure backend
    See https://github.com/apereo/cas/issues/2711

--- pac4j-oidc/src/main/java/org/pac4j/oidc/client/AzureAdClient.java
+++ pac4j-oidc/src/main/java/org/pac4j/oidc/client/AzureAdClient.java
@@ -17,14 +17,14 @@ import org.pac4j.oidc.profile.azuread.AzureAdProfileCreator;
  * Azure AD v2.0. Replace {@code tenantid} with the ID of the tenant to authenticate against. To
  * find this ID, fill in your tenant's domain name. Your tenant ID is the UUID in
  * {@code authorization_endpoint}.
- * 
+ *
  * For authentication against an unknown (or dynamic tenant), use {@code common} as ID.
  * Authentication against the common endpoint results in a ID token with a {@code issuer} different
  * from the {@code issuer} mentioned in the discovery data. This class uses to special validator
  * to correctly validate the issuer returned by Azure AD.
  *
  * More information at: https://msdn.microsoft.com/en-us/library/azure/dn645541.aspx
- * 
+ *
  * @author Emond Papegaaij
  * @since 1.8.3
  */
@@ -34,6 +34,9 @@ public class AzureAdClient extends OidcClient<AzureAdProfile> {
 
     public AzureAdClient(final OidcConfiguration configuration) {
         super(configuration);
+        // https://github.com/apereo/cas/issues/2711
+        // don't include clientName because its ignored and causes validation failure
+        setIncludeClientNameInCallbackUrl(false);
     }
 
     @Override
