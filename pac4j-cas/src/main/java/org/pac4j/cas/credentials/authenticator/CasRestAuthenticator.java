4980e7cdce4075864fe266e83e1c976afbd0b93f
Phan Nguyen <phannguyen@flix.comm.vn>
2017-06-29 15:29:26 +0700

    #949: return null profile when CAS authentication failed

--- pac4j-cas/src/main/java/org/pac4j/cas/credentials/authenticator/CasRestAuthenticator.java
+++ pac4j-cas/src/main/java/org/pac4j/cas/credentials/authenticator/CasRestAuthenticator.java
@@ -1,23 +1,27 @@
 package org.pac4j.cas.credentials.authenticator;
 
-import org.pac4j.cas.config.CasConfiguration;
-import org.pac4j.core.util.HttpUtils;
-import org.pac4j.cas.profile.CasRestProfile;
-import org.pac4j.core.context.HttpConstants;
-import org.pac4j.core.context.Pac4jConstants;
-import org.pac4j.core.context.WebContext;
-import org.pac4j.core.exception.HttpAction;
-import org.pac4j.core.exception.TechnicalException;
-import org.pac4j.core.credentials.UsernamePasswordCredentials;
-import org.pac4j.core.credentials.authenticator.Authenticator;
-import org.pac4j.core.util.CommonHelper;
-import org.pac4j.core.util.InitializableWebObject;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import org.pac4j.cas.config.CasConfiguration;
+import org.pac4j.cas.profile.CasRestProfile;
+import org.pac4j.core.context.HttpConstants;
+import org.pac4j.core.context.Pac4jConstants;
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.credentials.UsernamePasswordCredentials;
+import org.pac4j.core.credentials.authenticator.Authenticator;
+import org.pac4j.core.exception.HttpAction;
+import org.pac4j.core.exception.TechnicalException;
+import org.pac4j.core.util.CommonHelper;
+import org.pac4j.core.util.HttpUtils;
+import org.pac4j.core.util.InitializableWebObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Strings;
 
 /**
  * This is a specific Authenticator to deal with the CAS REST API.
@@ -26,6 +30,8 @@ import java.net.URL;
  * @since 1.8.0
  */
 public class CasRestAuthenticator extends InitializableWebObject implements Authenticator<UsernamePasswordCredentials> {
+
+	private final static Logger logger = LoggerFactory.getLogger(CasRestAuthenticator.class);
 
     private CasConfiguration configuration;
 
@@ -49,8 +55,13 @@ public class CasRestAuthenticator extends InitializableWebObject implements Auth
             throw new TechnicalException("Credentials are required");
         }
         final String ticketGrantingTicketId = requestTicketGrantingTicket(credentials.getUsername(), credentials.getPassword(), context);
-        final CasRestProfile profile = new CasRestProfile(ticketGrantingTicketId, credentials.getUsername());
-        credentials.setUserProfile(profile);
+		if (Strings.isNullOrEmpty(ticketGrantingTicketId)) {
+			credentials.setUserProfile(null);
+		} else {
+			final CasRestProfile profile = new CasRestProfile(ticketGrantingTicketId,
+					credentials.getUsername());
+			credentials.setUserProfile(profile);
+		}
     }
 
     private String requestTicketGrantingTicket(final String username, final String password, final WebContext context) {
@@ -70,8 +81,11 @@ public class CasRestAuthenticator extends InitializableWebObject implements Auth
                 return locationHeader.substring(locationHeader.lastIndexOf("/") + 1);
             }
 
-            throw new TechnicalException("Ticket granting ticket request failed: " + locationHeader + " " + responseCode +
+			logger.debug("Ticket granting ticket request failed: " + locationHeader + " "
+					+ responseCode +
                     HttpUtils.buildHttpErrorMessage(connection));
+
+			return null;
         } catch (final IOException e) {
             throw new TechnicalException(e);
         } finally {
