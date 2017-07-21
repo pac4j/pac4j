649dca56563e97adfb42f7dd7a47499d2495f2d8
Elie Roux <elie.roux@telecom-bretagne.eu>
2017-07-21 17:04:03 +0200

    Fixes InMemoryProfileService.update (#958)
    * attempt to fix #942
    
    * fix pmd error
    
    * fix class error
    
    * address comments
    
    * another batch of comments
    
    * tab -> spaces
    
    * 2.0 -> 2.1
    
    * fix nullpointerexception
    
    * fix InMemoryProfileService.update
    
    * format
    
    * update now creates profile if absent
    
    * change method as requested

--- pac4j-core/src/test/java/org/pac4j/core/profile/service/InMemoryProfileServiceTests.java
+++ pac4j-core/src/test/java/org/pac4j/core/profile/service/InMemoryProfileServiceTests.java
@@ -24,114 +24,125 @@ import static org.junit.Assert.*;
  */
 public final class InMemoryProfileServiceTests implements TestsConstants {
 
-	private static final String TEST_ID = "testId";
-	private static final String TEST_LINKED_ID = "testLinkedId";
-	private static final String TEST_USER = "testUser";
-	private static final String TEST_USER2 = "testUser2";
-	private static final String TEST_PASS = "testPass";
-	private static final String TEST_PASS2 = "testPass2";
-	private static final String IDPERSON1 = "idperson1";
-	private static final String IDPERSON2 = "idperson2";
-	private static final String IDPERSON3 = "idperson3";
+    private static final String TEST_ID = "testId";
+    private static final String TEST_LINKED_ID = "testLinkedId";
+    private static final String TEST_USER = "testUser";
+    private static final String TEST_USER2 = "testUser2";
+    private static final String TEST_PASS = "testPass";
+    private static final String TEST_PASS2 = "testPass2";
+    private static final String IDPERSON1 = "idperson1";
+    private static final String IDPERSON2 = "idperson2";
+    private static final String IDPERSON3 = "idperson3";
 
-	public final static PasswordEncoder PASSWORD_ENCODER = new ShiroPasswordEncoder(new DefaultPasswordService());
-	public InMemoryProfileService<CommonProfile> inMemoryProfileService;
+    public final static PasswordEncoder PASSWORD_ENCODER = new ShiroPasswordEncoder(new DefaultPasswordService());
+    public InMemoryProfileService<CommonProfile> inMemoryProfileService;
 
 
-	@Before
-	public void setUp() {
-		inMemoryProfileService = new InMemoryProfileService<CommonProfile>(x -> new CommonProfile());
-		inMemoryProfileService.setPasswordEncoder(PASSWORD_ENCODER);
-		final String password = PASSWORD_ENCODER.encode(PASSWORD);
-		// insert sample data
-		final Map<String, Object> properties1 = new HashMap<>();
-		properties1.put(USERNAME, GOOD_USERNAME);
-		properties1.put(FIRSTNAME, FIRSTNAME_VALUE);
-		CommonProfile profile = new CommonProfile();
-		profile.build(IDPERSON1, properties1);
-		inMemoryProfileService.create(profile, PASSWORD);
-		// second person, 
-		final Map<String, Object> properties2 = new HashMap<>();
-		properties2.put(USERNAME, MULTIPLE_USERNAME);
-		profile = new CommonProfile();
-		profile.build(IDPERSON2, properties2);
-		inMemoryProfileService.create(profile, PASSWORD);
-		final Map<String, Object> properties3 = new HashMap<>();
-		properties3.put(USERNAME, MULTIPLE_USERNAME);
-		properties3.put(PASSWORD, password);
-		profile = new CommonProfile();
-		profile.build(IDPERSON3, properties3);
-		inMemoryProfileService.create(profile, PASSWORD);
-	}
+    @Before
+    public void setUp() {
+        inMemoryProfileService = new InMemoryProfileService<CommonProfile>(x -> new CommonProfile());
+        inMemoryProfileService.setPasswordEncoder(PASSWORD_ENCODER);
+        final String password = PASSWORD_ENCODER.encode(PASSWORD);
+        // insert sample data
+        final Map<String, Object> properties1 = new HashMap<>();
+        properties1.put(USERNAME, GOOD_USERNAME);
+        properties1.put(FIRSTNAME, FIRSTNAME_VALUE);
+        CommonProfile profile = new CommonProfile();
+        profile.build(IDPERSON1, properties1);
+        inMemoryProfileService.create(profile, PASSWORD);
+        // second person, 
+        final Map<String, Object> properties2 = new HashMap<>();
+        properties2.put(USERNAME, MULTIPLE_USERNAME);
+        profile = new CommonProfile();
+        profile.build(IDPERSON2, properties2);
+        inMemoryProfileService.create(profile, PASSWORD);
+        final Map<String, Object> properties3 = new HashMap<>();
+        properties3.put(USERNAME, MULTIPLE_USERNAME);
+        properties3.put(PASSWORD, password);
+        profile = new CommonProfile();
+        profile.build(IDPERSON3, properties3);
+        inMemoryProfileService.create(profile, PASSWORD);
+    }
 
-	@Test(expected = AccountNotFoundException.class)
-	public void authentFailed() throws HttpAction, CredentialsException {
-		final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(BAD_USERNAME, PASSWORD, CLIENT_NAME);
-		inMemoryProfileService.validate(credentials, null);
-	}
+    @Test(expected = AccountNotFoundException.class)
+    public void authentFailed() throws HttpAction, CredentialsException {
+        final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(BAD_USERNAME, PASSWORD, CLIENT_NAME);
+        inMemoryProfileService.validate(credentials, null);
+    }
 
-	@Test
-	public void authentSuccessSingleAttribute() throws HttpAction, CredentialsException {
-		final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(GOOD_USERNAME, PASSWORD, CLIENT_NAME);
-		inMemoryProfileService.validate(credentials, null);
-		final CommonProfile profile = credentials.getUserProfile();
-		assertNotNull(profile);
-		assertEquals(GOOD_USERNAME, profile.getUsername());
-		assertEquals(2, profile.getAttributes().size());
-		assertEquals(FIRSTNAME_VALUE, profile.getAttribute(FIRSTNAME));
-	}
+    @Test
+    public void authentSuccessSingleAttribute() throws HttpAction, CredentialsException {
+        final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(GOOD_USERNAME, PASSWORD, CLIENT_NAME);
+        inMemoryProfileService.validate(credentials, null);
+        final CommonProfile profile = credentials.getUserProfile();
+        assertNotNull(profile);
+        assertEquals(GOOD_USERNAME, profile.getUsername());
+        assertEquals(2, profile.getAttributes().size());
+        assertEquals(FIRSTNAME_VALUE, profile.getAttribute(FIRSTNAME));
+    }
 
-	@Test
-	public void testCreateUpdateFindDelete() throws HttpAction, CredentialsException {
-		final CommonProfile profile = new CommonProfile();
-		profile.setId(TEST_ID);
-		profile.setLinkedId(TEST_LINKED_ID);
-		profile.addAttribute(USERNAME, TEST_USER);
-		// create
-		inMemoryProfileService.create(profile, TEST_PASS);
-		// check credentials
-		final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(TEST_USER, TEST_PASS, CLIENT_NAME);
-		inMemoryProfileService.validate(credentials, null);
-		final CommonProfile profile1 = credentials.getUserProfile();
-		assertNotNull(profile1);
-		// check data
-		final List<Map<String, Object>> results = getData(TEST_ID);
-		assertEquals(1, results.size());
-		final Map<String, Object> result = results.get(0);
-		assertEquals(5, result.size());
-		assertEquals(TEST_ID, result.get("id"));
-		assertEquals(TEST_LINKED_ID, result.get(AbstractProfileService.LINKEDID));
-		assertNotNull(result.get(AbstractProfileService.SERIALIZED_PROFILE));
-		assertEquals(TEST_USER, result.get(USERNAME));
-		// findById
-		final CommonProfile profile2 = inMemoryProfileService.findById(TEST_ID);
-		assertEquals(TEST_ID, profile2.getId());
-		assertEquals(TEST_LINKED_ID, profile2.getLinkedId());
-		assertEquals(TEST_USER, profile2.getUsername());
-		assertEquals(1, profile2.getAttributes().size());
-		// update
-		profile.addAttribute(USERNAME, TEST_USER2);
-		inMemoryProfileService.update(profile, TEST_PASS2);
-		final List<Map<String, Object>> results2 = getData(TEST_ID);
-		assertEquals(1, results2.size());
-		final Map<String, Object> result2 = results2.get(0);
-		assertEquals(5, result2.size());
-		assertEquals(TEST_ID, result2.get("id"));
-		assertEquals(TEST_LINKED_ID, result2.get(AbstractProfileService.LINKEDID));
-		assertNotNull(result2.get(AbstractProfileService.SERIALIZED_PROFILE));
-		assertEquals(TEST_USER2, result2.get(USERNAME));
-		// check credentials
-		final UsernamePasswordCredentials credentials2 = new UsernamePasswordCredentials(TEST_USER2, TEST_PASS2, CLIENT_NAME);
-		inMemoryProfileService.validate(credentials2, null);
-		final CommonProfile profile3 = credentials.getUserProfile();
-		assertNotNull(profile3);
-		// remove
-		inMemoryProfileService.remove(profile);
-		final List<Map<String, Object>> results3 = getData(TEST_ID);
-		assertEquals(0, results3.size());
-	}
+    @Test
+    public void testCreateUpdateFindDelete() throws HttpAction, CredentialsException {
+        final CommonProfile profile = new CommonProfile();
+        profile.setId(TEST_ID);
+        profile.setLinkedId(TEST_LINKED_ID);
+        profile.addAttribute(USERNAME, TEST_USER);
+        // create
+        inMemoryProfileService.create(profile, TEST_PASS);
+        // check credentials
+        final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(TEST_USER, TEST_PASS, CLIENT_NAME);
+        inMemoryProfileService.validate(credentials, null);
+        final CommonProfile profile1 = credentials.getUserProfile();
+        assertNotNull(profile1);
+        // check data
+        final List<Map<String, Object>> results = getData(TEST_ID);
+        assertEquals(1, results.size());
+        final Map<String, Object> result = results.get(0);
+        assertEquals(5, result.size());
+        assertEquals(TEST_ID, result.get("id"));
+        assertEquals(TEST_LINKED_ID, result.get(AbstractProfileService.LINKEDID));
+        assertNotNull(result.get(AbstractProfileService.SERIALIZED_PROFILE));
+        assertEquals(TEST_USER, result.get(USERNAME));
+        // findById
+        final CommonProfile profile2 = inMemoryProfileService.findById(TEST_ID);
+        assertEquals(TEST_ID, profile2.getId());
+        assertEquals(TEST_LINKED_ID, profile2.getLinkedId());
+        assertEquals(TEST_USER, profile2.getUsername());
+        assertEquals(1, profile2.getAttributes().size());
+        // update with password
+        profile.addAttribute(USERNAME, TEST_USER2);
+        inMemoryProfileService.update(profile, TEST_PASS2);
+        List<Map<String, Object>> results2 = getData(TEST_ID);
+        assertEquals(1, results2.size());
+        Map<String, Object> result2 = results2.get(0);
+        assertEquals(5, result2.size());
+        assertEquals(TEST_ID, result2.get("id"));
+        assertEquals(TEST_LINKED_ID, result2.get(AbstractProfileService.LINKEDID));
+        assertNotNull(result2.get(AbstractProfileService.SERIALIZED_PROFILE));
+        assertEquals(TEST_USER2, result2.get(USERNAME));
+        // check credentials
+        final UsernamePasswordCredentials credentials2 = new UsernamePasswordCredentials(TEST_USER2, TEST_PASS2, CLIENT_NAME);
+        inMemoryProfileService.validate(credentials2, null);
+        final CommonProfile profile3 = credentials.getUserProfile();
+        assertNotNull(profile3);
+        // update with no password update
+        inMemoryProfileService.update(profile, null);
+        results2 = getData(TEST_ID);
+        assertEquals(1, results2.size());
+        result2 = results2.get(0);
+        assertEquals(5, result2.size());
+        assertEquals(TEST_USER2, result2.get(USERNAME));
+        // check credentials
+        inMemoryProfileService.validate(credentials2, null);
+        final CommonProfile profile4 = credentials.getUserProfile();
+        assertNotNull(profile4);
+        // remove
+        inMemoryProfileService.remove(profile);
+        final List<Map<String, Object>> results3 = getData(TEST_ID);
+        assertEquals(0, results3.size());
+    }
 
-	private List<Map<String, Object>> getData(final String id) {
-		return inMemoryProfileService.read(Arrays.asList("id", "username", "linkedid", "password", "serializedprofile"), "id", id);
-	}
+    private List<Map<String, Object>> getData(final String id) {
+        return inMemoryProfileService.read(Arrays.asList("id", "username", "linkedid", "password", "serializedprofile"), "id", id);
+    }
 }
