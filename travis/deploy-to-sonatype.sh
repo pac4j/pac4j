6bb7f9c9c8caf5fe91f8a912b8e566b69d603cac
leleuj <leleuj@gmail.com>
2017-07-27 09:54:37 +0200

    all branches accepted, except gh-pages

--- travis/deploy-to-sonatype.sh
+++ travis/deploy-to-sonatype.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Only invoke the deployment to Sonatype when it's not a PR and only for master
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+# Only invoke the deployment to Sonatype when it's not a PR and not gh-pages
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" != "gh-pages" ]; then
   mvn deploy -DskipTests --settings travis/settings.xml
   echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
 fi
