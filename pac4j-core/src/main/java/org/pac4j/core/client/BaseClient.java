de2b25b8ffa4b0f7f474330c51878b0bc21132bf
leleuj <leleuj@gmail.com>
2017-09-02 08:40:26 +0200

    timing on credentials validation

--- pac4j-core/src/main/java/org/pac4j/core/client/BaseClient.java
+++ pac4j-core/src/main/java/org/pac4j/core/client/BaseClient.java
@@ -22,15 +22,15 @@ import org.slf4j.LoggerFactory;
 /**
  * <p>This class is the default implementation of an authentication client (whatever the mechanism). It has the core concepts:</p>
  * <ul>
- * <li>The initialization process is handled by the {@link InitializableWebObject} inheritance, the {@link #internalInit(WebContext)} 
- * must be implemented in sub-classes. The {@link #init(WebContext)} method must be called implicitly by the main methods of 
+ * <li>The initialization process is handled by the {@link InitializableWebObject} inheritance, the {@link #internalInit(WebContext)}
+ * must be implemented in sub-classes. The {@link #init(WebContext)} method must be called implicitly by the main methods of
  * the {@link Client} interface, so that no explicit call is required to initialize the client</li>
  * <li>The name of the client is handled through the {@link #setName(String)} and {@link #getName()} methods</li>
- * <li>After retrieving the user profile, the client can generate the authorization information (roles, permissions and remember-me) by 
+ * <li>After retrieving the user profile, the client can generate the authorization information (roles, permissions and remember-me) by
  * using the appropriate {@link AuthorizationGenerator}</li>
- * <li>The credentials extraction and validation in the {@link #getCredentials(WebContext)} method are handled by the 
- * {@link #credentialsExtractor} and {@link #authenticator} components</li> 
- * <li>The user profile retrieval in the {@link #getUserProfile(Credentials, WebContext)} method is ensured by the {@link #profileCreator} 
+ * <li>The credentials extraction and validation in the {@link #getCredentials(WebContext)} method are handled by the
+ * {@link #credentialsExtractor} and {@link #authenticator} components</li>
+ * <li>The user profile retrieval in the {@link #getUserProfile(Credentials, WebContext)} method is ensured by the {@link #profileCreator}
  * component.</li>
  * </ul>
  *
@@ -64,7 +64,13 @@ public abstract class BaseClient<C extends Credentials, U extends CommonProfile>
             if (credentials == null) {
                 return null;
             }
-            this.authenticator.validate(credentials, context);
+            final long t0 = System.currentTimeMillis();
+            try {
+                this.authenticator.validate(credentials, context);
+            } finally {
+                final long t1 = System.currentTimeMillis();
+                logger.debug("Credentials validation took: {} ms", t1 - t0);
+            }
             return credentials;
         } catch (CredentialsException e) {
             logger.info("Failed to retrieve or validate credentials: {}", e.getMessage());
@@ -144,7 +150,7 @@ public abstract class BaseClient<C extends Credentials, U extends CommonProfile>
 
     /**
      * Add an authorization generator.
-     * 
+     *
      * @param authorizationGenerator an authorizations generator
      */
     public void setAuthorizationGenerator(final AuthorizationGenerator<U> authorizationGenerator) {
@@ -206,7 +212,7 @@ public abstract class BaseClient<C extends Credentials, U extends CommonProfile>
     @Override
     public String toString() {
         return CommonHelper.toString(this.getClass(), "name", getName(), "credentialsExtractor", this.credentialsExtractor,
-                "authenticator", this.authenticator, "profileCreator", this.profileCreator, 
+                "authenticator", this.authenticator, "profileCreator", this.profileCreator,
                 "authorizationGenerators", authorizationGenerators);
     }
 }
