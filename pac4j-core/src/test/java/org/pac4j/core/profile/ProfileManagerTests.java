d6c65f8cfaa6801fde269f30b10109b2e834052c
leleuj <leleuj@gmail.com>
2017-09-14 05:11:25 +0200

    Update documentation

--- pac4j-core/src/test/java/org/pac4j/core/profile/ProfileManagerTests.java
+++ pac4j-core/src/test/java/org/pac4j/core/profile/ProfileManagerTests.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.pac4j.core.context.MockWebContext;
 import org.pac4j.core.context.Pac4jConstants;
-import org.pac4j.core.context.WebContext;
 
 import java.util.LinkedHashMap;
 import java.util.List;
