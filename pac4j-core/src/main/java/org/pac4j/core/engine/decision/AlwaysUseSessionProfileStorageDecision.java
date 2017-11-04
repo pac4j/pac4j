ccc72b7c45892182306edefcfc39a9e3578f60aa
LELEU Jérôme <leleuj@gmail.com>
2017-11-04 15:06:23 +0100

    loadProfileFromSession (#1019)
    * loadProfileFromSession
    
    * renaming + default values
    
    * extract the loadProfilesFromSession and saveProfileInSession methods into a ProfileStorageStrategy
    
    * Strategy -> Decision
    
    * update documentation

---
+++ pac4j-core/src/main/java/org/pac4j/core/engine/decision/AlwaysUseSessionProfileStorageDecision.java
@@ -0,0 +1,28 @@
+package org.pac4j.core.engine.decision;
+
+import org.pac4j.core.client.Client;
+import org.pac4j.core.client.DirectClient;
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.profile.CommonProfile;
+
+import java.util.List;
+
+/**
+ * A decision class where the session is always used, generally when indirect and direct clients are mixed in the same web application.
+ *
+ * @author Jerome Leleu
+ * @since 3.0.0
+ */
+public class AlwaysUseSessionProfileStorageDecision<C extends WebContext> implements ProfileStorageDecision<C> {
+
+    @Override
+    public boolean mustLoadProfilesFromSession(final C context, final List<Client> currentClients) {
+        return true;
+    }
+
+    @Override
+    public boolean mustSaveProfileInSession(final C context, final List<Client> currentClients,
+                                            final DirectClient directClient, final CommonProfile profile) {
+        return true;
+    }
+}
