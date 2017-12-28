dc7f417516f09295ea2abe94ae8b3a944092f978
tbcheng <tbcheng89@gmail.com>
2017-12-28 09:40:13 +0800

    Retain the field value of the non-character type in the profile (#1075)

--- pac4j-core/src/main/java/org/pac4j/core/profile/converter/StringConverter.java
+++ pac4j-core/src/main/java/org/pac4j/core/profile/converter/StringConverter.java
@@ -2,7 +2,7 @@ package org.pac4j.core.profile.converter;
 
 /**
  * This class only keeps String objects.
- * 
+ *
  * @author Jerome Leleu
  * @since 1.1.0
  */
@@ -10,5 +10,14 @@ public final class StringConverter extends AbstractAttributeConverter<String> {
 
     public StringConverter() {
         super(String.class);
+    }
+
+    @Override
+    protected String internalConvert(final Object attribute) {
+        if (null != attribute) {
+            return attribute.toString();
+        } else {
+            return null;
+        }
     }
 }
