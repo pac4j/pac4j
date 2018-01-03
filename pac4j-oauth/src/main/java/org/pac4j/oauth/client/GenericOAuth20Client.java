68fa388a86b33f1666ac21c7b95215dfed34dc3c
汤波成 <512882840@qq.com>
2018-01-03 14:43:34 +0800

    fix(#1075) set the appropriate converter per profile attribute
    Compatible with the previous configuration

--- pac4j-oauth/src/main/java/org/pac4j/oauth/client/GenericOAuth20Client.java
+++ pac4j-oauth/src/main/java/org/pac4j/oauth/client/GenericOAuth20Client.java
@@ -1,9 +1,16 @@
 package org.pac4j.oauth.client;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Vector;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 import com.github.scribejava.core.model.Verb;
+import org.pac4j.core.profile.converter.AbstractAttributeConverter;
 import org.pac4j.oauth.profile.OAuth20Profile;
 import org.pac4j.oauth.profile.generic.GenericOAuth20ProfileDefinition;
 import org.pac4j.scribe.builder.api.GenericApi20;
@@ -26,6 +33,7 @@ public class GenericOAuth20Client extends OAuth20Client<OAuth20Profile> {
     private Verb profileVerb;
     private Map<String, String> profileAttrs;
     private Map<String, String> customParams;
+    private Class[] converterClasses;
 
     public GenericOAuth20Client() {
     }
@@ -46,14 +54,66 @@ public class GenericOAuth20Client extends OAuth20Client<OAuth20Profile> {
         profileDefinition.setProfileUrl(profileUrl);
 
         if (profileAttrs != null) {
-            for (Map.Entry<String,String> entry : profileAttrs.entrySet()) {
-                profileDefinition.profileAttribute(entry.getKey(), entry.getValue(), null);
+            for (Map.Entry<String, String> entry : profileAttrs.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                String[] tokens = value.split("\\|");
+                if (tokens.length == 2) {
+                    profileDefinition.profileAttribute(key, tokens[1], getConverter(tokens[0]));
+                } else if (tokens.length == 1) {
+                    profileDefinition.profileAttribute(key, value, null);
+                } else {
+                    LOG.warning("Ignored incorrect attribute value expressions:" + value);
+                }
             }
         }
 
         configuration.setProfileDefinition(profileDefinition);
 
         super.clientInit();
+    }
+
+    private Class[] getConverters() {
+        if (converterClasses == null) {
+            try {
+                ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+                Class cla = classLoader.getClass();
+                while (cla != ClassLoader.class) {
+                    cla = cla.getSuperclass();
+                }
+                Field field = cla.getDeclaredField("classes");
+                field.setAccessible(true);
+                Vector<Class> classes = (Vector<Class>) field.get(classLoader);
+                converterClasses = classes.stream()
+                    .filter(x -> AbstractAttributeConverter.class.isAssignableFrom(x) && !Modifier.isAbstract(x.getModifiers()))
+                    .toArray(Class[]::new);
+                return converterClasses;
+            } catch (Exception e) {
+                LOG.warning(e.toString());
+            }
+        }
+        return converterClasses;
+    }
+
+    private AbstractAttributeConverter getConverter(final String typeName) {
+        try {
+            Stream<Class> acceptableConverters = Arrays.stream(getConverters())
+                .filter(x -> {
+                    try {
+                        AbstractAttributeConverter<?> converter = (AbstractAttributeConverter<?>) x.newInstance();
+                        Method accept = AbstractAttributeConverter.class.getDeclaredMethod("accept", String.class);
+                        return (Boolean) accept.invoke(converter, typeName);
+                    } catch (Exception e) {
+                        LOG.warning("Ignore type which no parameterless constructor:" + x.getName());
+                    }
+                    return false;
+                });
+            Class converterClazz = acceptableConverters.findFirst().get();
+            return (AbstractAttributeConverter<?>) converterClazz.newInstance();
+        } catch (Exception e) {
+            LOG.warning(e.toString());
+        }
+        return null;
     }
 
     public void setAuthUrl(final String authUrl) {
