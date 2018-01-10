91181d4f6d5a49cb0a67453918f8cd9109061501
Jérôme LELEU <leleuj@gmail.com>
2018-01-10 11:39:19 +0100

    Find the client when we only have one

--- pac4j-core/src/main/java/org/pac4j/core/client/finder/DefaultCallbackClientFinder.java
+++ pac4j-core/src/main/java/org/pac4j/core/client/finder/DefaultCallbackClientFinder.java
@@ -22,10 +22,12 @@ public class DefaultCallbackClientFinder implements ClientFinder {
     public List<Client> find(final Clients clients, final WebContext context, final String clientNames) {
 
         final List<Client> result = new ArrayList<>();
+        final List<Client> indirectClients = new ArrayList<>();
 
         for (final Client client : clients.findAllClients()) {
             if (client instanceof IndirectClient) {
                 final IndirectClient indirectClient = (IndirectClient) client;
+                indirectClients.add(client);
                 indirectClient.init();
                 if (indirectClient.getCallbackUrlResolver().matches(indirectClient.getName(), context)) {
                     result.add(indirectClient);
@@ -33,9 +35,15 @@ public class DefaultCallbackClientFinder implements ClientFinder {
             }
         }
 
-        // fallback: we didn't find any client on the URL and have a default client
-        if (result.isEmpty() && CommonHelper.isNotBlank(clientNames)) {
-            result.add(clients.findClient(clientNames));
+        // fallback: we didn't find any client on the URL
+        if (result.isEmpty()) {
+            //  we have a default client, use it
+            if (CommonHelper.isNotBlank(clientNames)) {
+                result.add(clients.findClient(clientNames));
+                // or we only have one indirect client, use it
+            } else if (indirectClients.size() == 1){
+                result.addAll(indirectClients);
+            }
         }
 
         return result;
