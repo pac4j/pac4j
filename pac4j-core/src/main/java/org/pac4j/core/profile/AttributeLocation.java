4c1933c6ddd6502685fb31ac2c4b033593532f64
Jarda Kacer <jaroslav@kacer.biz>
2018-02-23 14:15:05 +0100

    #1103: Check how authentication attributes are restored...
    The Profile Helper and Profile Definition now accept both profile
    attributes and authentication attributes when restoring a profile.
    
    Older methods with support for profile attributes only were kept for
    backward compatibility.

---
+++ pac4j-core/src/main/java/org/pac4j/core/profile/AttributeLocation.java
@@ -0,0 +1,16 @@
+package org.pac4j.core.profile;
+
+
+/**
+ * Denotes where an attribute is placed in a profile.
+ * 
+ * @author jkacer
+ */
+public enum AttributeLocation {
+
+    /** Profile "basic" attribute. */
+    PROFILE_ATTRIBUTE,
+
+    /** Profile authentication attribute. */
+    AUTHENTICATION_ATTRIBUTE;
+}
