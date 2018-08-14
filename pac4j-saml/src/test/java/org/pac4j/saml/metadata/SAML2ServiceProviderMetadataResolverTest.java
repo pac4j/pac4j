7bc05b3a5f4bf3bb502956bbaeb39e9d9c9b843d
Misagh Moayyed <mmoayyed@unicon.net>
2018-08-14 10:22:23 +0430

    init service provider metadata at construction time

--- pac4j-saml/src/test/java/org/pac4j/saml/metadata/SAML2ServiceProviderMetadataResolverTest.java
+++ pac4j-saml/src/test/java/org/pac4j/saml/metadata/SAML2ServiceProviderMetadataResolverTest.java
@@ -4,22 +4,39 @@ import org.junit.Before;
 import org.junit.Test;
 import org.pac4j.core.exception.TechnicalException;
 import org.pac4j.saml.client.SAML2ClientConfiguration;
+import org.pac4j.saml.crypto.KeyStoreCredentialProvider;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
+
+import static org.junit.Assert.*;
 
 public class SAML2ServiceProviderMetadataResolverTest {
 
     private SAML2ServiceProviderMetadataResolver metadataResolver;
 
+    private SAML2ClientConfiguration configuration;
+
     @Before
     public void setUp() {
-        SAML2ClientConfiguration configuration = new SAML2ClientConfiguration();
-        configuration.setServiceProviderMetadataResource(new FileSystemResource("target"));
+        configuration = new SAML2ClientConfiguration();
+        configuration.setKeystorePath("target/keystore.jks");
+        configuration.setKeystorePassword("pac4j");
+        configuration.setPrivateKeyPassword("pac4j");
         configuration.setServiceProviderMetadataResource(new FileSystemResource("target/out.xml"));
-        metadataResolver = new SAML2ServiceProviderMetadataResolver(configuration, "http://localhost", null);
+        configuration.setIdentityProviderMetadataResource(new ClassPathResource("idp-metadata.xml"));
+        configuration.init();
     }
 
     @Test(expected = TechnicalException.class)
     public void resolveShouldThrowExceptionIfCredentialsProviderIsNullAndAuthnRequestSignedIsTrue() {
+        metadataResolver = new SAML2ServiceProviderMetadataResolver(configuration, "http://localhost", null);
         metadataResolver.resolve();
+    }
+
+    @Test
+    public void resolveServiceProviderMetadata() {
+        metadataResolver = new SAML2ServiceProviderMetadataResolver(configuration, "http://localhost", new KeyStoreCredentialProvider(configuration));
+        assertTrue(configuration.getServiceProviderMetadataResource().exists());
+        assertNotNull(metadataResolver.resolve());
     }
 }
