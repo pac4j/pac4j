61958aeb63d082625bc8c6f2c23eb63cf31e8e6d
LELEU Jérôme <leleuj@gmail.com>
2019-03-11 14:31:05 +0100

    Improve CAS support (#1276)
    * Improve CAS support
    
    * update Javadoc

--- pac4j-cas/src/main/java/org/pac4j/cas/redirect/CasRedirectActionBuilder.java
+++ pac4j-cas/src/main/java/org/pac4j/cas/redirect/CasRedirectActionBuilder.java
@@ -1,8 +1,10 @@
 package org.pac4j.cas.redirect;
 
+import org.jasig.cas.client.Protocol;
 import org.jasig.cas.client.util.CommonUtils;
 import org.pac4j.cas.client.CasClient;
 import org.pac4j.cas.config.CasConfiguration;
+import org.pac4j.cas.config.CasProtocol;
 import org.pac4j.core.redirect.RedirectAction;
 import org.pac4j.core.context.WebContext;
 import org.pac4j.core.redirect.RedirectActionBuilder;
@@ -33,11 +35,23 @@ public class CasRedirectActionBuilder implements RedirectActionBuilder {
 
     @Override
     public RedirectAction redirect(final WebContext context) {
-        final String computeLoginUrl = configuration.computeFinalLoginUrl(context);
+        String computeLoginUrl = configuration.computeFinalLoginUrl(context);
+        final String method = configuration.getMethod();
+        if (method != null) {
+            computeLoginUrl = CommonHelper.addParameter(computeLoginUrl, "method", method);
+        }
         final String computedCallbackUrl = client.computeFinalCallbackUrl(context);
-        final String redirectionUrl = CommonUtils.constructRedirectUrl(computeLoginUrl, CasConfiguration.SERVICE_PARAMETER,
+        final String redirectionUrl = CommonUtils.constructRedirectUrl(computeLoginUrl, getServiceParameter(),
                 computedCallbackUrl, configuration.isRenew(), configuration.isGateway());
         logger.debug("redirectionUrl: {}", redirectionUrl);
         return RedirectAction.redirect(redirectionUrl);
+    }
+
+    private String getServiceParameter() {
+        if (configuration.getProtocol() == CasProtocol.SAML) {
+            return Protocol.SAML11.getServiceParameterName();
+        } else {
+            return CasConfiguration.SERVICE_PARAMETER;
+        }
     }
 }
