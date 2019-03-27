9ad7219372f371b4f79050d78e4ac2f2e629bd1b
davoustp <pascal.davoust@eptica.com>
2019-03-27 08:42:03 +0100

    pac4j-kerberos does not implement SPNEGO with DirectClient (#1294) (#1295)
    * pac4j-kerberos does not implement SPNEGO with DirectClient (#1294)
    
    * pac4j-kerberos does not implement SPNEGO with DirectClient (#1294)

--- pac4j-kerberos/src/test/java/org/pac4j/kerberos/client/direct/KerberosClientTests.java
+++ pac4j-kerberos/src/test/java/org/pac4j/kerberos/client/direct/KerberosClientTests.java
@@ -73,6 +73,16 @@ public class KerberosClientTests implements TestsConstants {
     }
 
     @Test
+    public void testWWWAuthenticateNegotiateHeaderIsSetToTriggerSPNEGOWhenNoCredentialsAreFound() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        final DirectKerberosClient client = new DirectKerberosClient(new KerberosAuthenticator(krbValidator));
+        final Optional<KerberosCredentials> credentials = client.getCredentials(new JEEContext(request, response));
+        assertFalse(credentials.isPresent());
+        verify(response).setHeader(HttpConstants.AUTHENTICATE_HEADER, "Negotiate");
+    }
+
+    @Test
     public void testAuthentication() {
         when(krbValidator.validateTicket(any())).thenReturn(new KerberosTicketValidation("garry", null, null, null));
         final DirectKerberosClient client = new DirectKerberosClient(new KerberosAuthenticator(krbValidator));
