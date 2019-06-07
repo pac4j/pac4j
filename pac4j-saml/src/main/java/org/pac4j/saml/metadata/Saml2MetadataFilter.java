7d900ac01638cc641c3282034ec70abd0ab8b17e
minfrin <minfrin@users.noreply.github.com>
2019-06-07 09:07:17 +0100

    Rename client to clientName.

--- pac4j-saml/src/main/java/org/pac4j/saml/metadata/Saml2MetadataFilter.java
+++ pac4j-saml/src/main/java/org/pac4j/saml/metadata/Saml2MetadataFilter.java
@@ -21,7 +21,7 @@ import org.pac4j.saml.client.SAML2Client;
  * 
  * saml2MetadataFilter = org.pac4j.saml.metadata.Saml2MetadataFilter
  * saml2MetadataFilter.config = $config
- * saml2MetadataFilter.client = SAML2Client
+ * saml2MetadataFilter.clientName = SAML2Client
  * 
  * [urls]
  * /API/SAML2/metadata = saml2MetadataFilter
@@ -33,7 +33,7 @@ public class Saml2MetadataFilter implements Filter {
 
     private Config config;
 
-    private String client;
+    private String clientName;
 
     @Override
     public void init(final FilterConfig filterConfig) throws ServletException {
@@ -44,15 +44,15 @@ public class Saml2MetadataFilter implements Filter {
             final FilterChain filterChain) throws IOException, ServletException {
 
         CommonHelper.assertNotNull("config", config);
-        CommonHelper.assertNotNull("client", client);
+        CommonHelper.assertNotNull("clientName", clientName);
 
-        SAML2Client client = (SAML2Client) config.getClients().findClient(this.client);
+        SAML2Client client = (SAML2Client) config.getClients().findClient(this.clientName);
         if (client != null) {
             client.init();
             servletResponse.getWriter().write(client.getServiceProviderMetadataResolver().getMetadata());
             servletResponse.getWriter().flush();
         } else {
-            throw new TechnicalException("No SAML2Client: " + this.client);
+            throw new TechnicalException("No SAML2Client: " + this.clientName);
         }
     }
 
@@ -68,12 +68,12 @@ public class Saml2MetadataFilter implements Filter {
         this.config = config;
     }
 
-    public String getClient() {
-        return client;
+    public String getClientName() {
+        return clientName;
     }
 
-    public void setClient(String client) {
-        this.client = client;
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
     }
 
 }
