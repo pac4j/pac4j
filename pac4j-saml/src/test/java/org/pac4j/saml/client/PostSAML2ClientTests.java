cfc41c18058b20cfc927db406674a35e91949714
Emond Papegaaij <emond.papegaaij@topicus.nl>
2019-06-25 07:21:00 +0200

    Build on jdk11 (#1325)
    * Java 11 buildfixes
    
    * Move to spotbugs
    
    * Fix tests for XML content rendered different order

--- pac4j-saml/src/test/java/org/pac4j/saml/client/PostSAML2ClientTests.java
+++ pac4j-saml/src/test/java/org/pac4j/saml/client/PostSAML2ClientTests.java
@@ -32,11 +32,19 @@ public final class PostSAML2ClientTests extends AbstractSAML2ClientTests {
         client.getConfiguration().setUseNameQualifier(true);
         final WebContext context = new J2EContext(new MockHttpServletRequest(), new MockHttpServletResponse());
         final RedirectAction action = client.getRedirectAction(context);
-        assertTrue(getDecodedAuthnRequest(action.getContent())
-                .contains("<saml2:Issuer "
-                        + "Format=\"urn:oasis:names:tc:SAML:2.0:nameid-format:entity\" "
-                        + "NameQualifier=\"http://localhost:8080/cb\" "
-                        + "xmlns:saml2=\"urn:oasis:names:tc:SAML:2.0:assertion\">http://localhost:8080/cb</saml2:Issuer>"));
+        
+        // JDK8 and JDK11 do not produce the same XML (attributes in different order)
+        // something like xmlunit would have been better but may be a bit overkill for just 2 failing tests
+        final String issuerJdk8 = "<saml2:Issuer "
+                + "Format=\"urn:oasis:names:tc:SAML:2.0:nameid-format:entity\" "
+                + "NameQualifier=\"http://localhost:8080/cb\" "
+                + "xmlns:saml2=\"urn:oasis:names:tc:SAML:2.0:assertion\">http://localhost:8080/cb</saml2:Issuer>";
+        final String issuerJdk11 = "<saml2:Issuer "
+                + "xmlns:saml2=\"urn:oasis:names:tc:SAML:2.0:assertion\" "
+                + "Format=\"urn:oasis:names:tc:SAML:2.0:nameid-format:entity\" "
+                + "NameQualifier=\"http://localhost:8080/cb\">http://localhost:8080/cb</saml2:Issuer>";
+        final String decodedAuthnRequest = getDecodedAuthnRequest(action.getContent());
+        assertTrue(decodedAuthnRequest.contains(issuerJdk8) || decodedAuthnRequest.contains(issuerJdk11));
     }
 
     @Test
