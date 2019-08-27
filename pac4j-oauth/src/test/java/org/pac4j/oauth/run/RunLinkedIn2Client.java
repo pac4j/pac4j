7660ac94cb49b96fb7b9e844bab20f5cc758930c
Vassilis Virvilis <vasvir2@gmail.com>
2019-08-28 00:49:44 +0300

    #1266 LinkedIn v2 API support

--- pac4j-oauth/src/test/java/org/pac4j/oauth/run/RunLinkedIn2Client.java
+++ pac4j-oauth/src/test/java/org/pac4j/oauth/run/RunLinkedIn2Client.java
@@ -9,8 +9,6 @@ import org.pac4j.core.util.CommonHelper;
 import org.pac4j.oauth.client.LinkedIn2Client;
 import org.pac4j.oauth.profile.linkedin2.*;
 
-import java.util.List;
-
 import static org.junit.Assert.*;
 
 /**
@@ -38,9 +36,9 @@ public final class RunLinkedIn2Client extends RunClient {
     @Override
     protected IndirectClient getClient() {
         final LinkedIn2Client client = new LinkedIn2Client();
-        client.setKey("gsqj8dn56ayn");
-        client.setSecret("kUFAZ2oYvwMQ6HFl");
-        client.setScope("r_basicprofile r_emailaddress rw_company_admin w_share");
+        client.setKey("86xtdkj897xk7d");
+        client.setSecret("lU5w96OOs4hJ7HqU");
+        client.setScope("r_liteprofile r_emailaddress");
         client.setCallbackUrl(PAC4J_URL);
         return client;
     }
@@ -68,32 +66,6 @@ public final class RunLinkedIn2Client extends RunClient {
                 null,
                 "https://media.licdn.com/dms/image/C5603AQHfitHQ5I8fcg/profile-displayphoto-shrink_100_100/" +
                     "0?e=15",
-                "http://www.linkedin.com/in/test-scribeup-16b0aa48", "Paris Area, France");
-        final LinkedIn2Location location = profile.getCompleteLocation();
-        assertEquals("Paris Area, France", location.getName());
-        assertNull(profile.getMaidenName());
-        assertEquals("ScribeUP développeur chez OpenSource", profile.getHeadline());
-        assertEquals("Information Technology and Services", profile.getIndustry());
-        assertEquals(1, profile.getNumConnections().intValue());
-        assertEquals("This is a summary...", profile.getSummary());
-        assertNull(profile.getSpecialties());
-        final List<LinkedIn2Position> positions = profile.getPositions();
-        assertEquals(2, positions.size());
-        final LinkedIn2Position position = positions.get(0);
-        assertEquals("417494299", position.getId());
-        assertEquals("Developer", position.getTitle());
-        assertEquals("Desc", position.getSummary());
-        final LinkedIn2Date startDate = position.getStartDate();
-        assertEquals(2012, startDate.getYear().intValue());
-        assertEquals(3, startDate.getMonth().intValue());
-        assertNull(position.getEndDate());
-        final LinkedIn2Company company = position.getCompany();
-        assertEquals("PAC4J", company.getName());
-        assertNull(company.getIndustry());
-        assertEquals("https://www.linkedin.com/profile/view?id=AAoAAAn67mMBxVIxeJXn2T6XBvOFEAMLv7RiJQQ"
-                + "&authType=name&authToken=_IWF&trk=api*a167383*s175634*",
-                profile.getSiteStandardProfileRequest());
-        assertEquals("AAoAAAn67mMBxVIxeJXn2T6XBvOFEAMLv7RiJQQ", profile.getOAuth10Id());
-        assertEquals(16, profile.getAttributes().size());
+                null, null);
     }
 }
