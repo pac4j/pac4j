2770e90be6ba8b7da09dc8c4aaffa4d24287f5b9
Vassilis Virvilis <vasvir2@gmail.com>
2019-09-13 08:49:26 +0300

    FigShare support for 3.8.x (#1382)
    * #1266 LinkedIn v2 API support
    
    * #1266 LinkedIn v2 API support: try 2
    
    * FigShare support for 3.8.x
    
    * import cleanup, release notes addition
    
    * PMD fixes
    
    * base FigShareClient on OAuth20Client
    
    * undo changes to GenericOAuth20ProfileDefinition
    
    * remove unneeded secondary attributes handling
    
    * style: organize imports

---
+++ pac4j-oauth/src/main/java/org/pac4j/scribe/builder/api/FigShareApi20.java
@@ -0,0 +1,48 @@
+package org.pac4j.scribe.builder.api;
+
+import java.io.OutputStream;
+
+import com.github.scribejava.core.builder.api.DefaultApi20;
+import com.github.scribejava.core.httpclient.HttpClient;
+import com.github.scribejava.core.httpclient.HttpClientConfig;
+import com.github.scribejava.core.model.OAuthConstants;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.oauth.OAuth20Service;
+
+/**
+*
+* @author Vassilis Virvilis
+* @since 3.8.0
+*/
+public class FigShareApi20 extends DefaultApi20 {
+    public static class Service extends OAuth20Service {
+        public Service(DefaultApi20 api, String apiKey, String apiSecret, String callback, String scope, String state, String responseType,
+                String userAgent, HttpClientConfig httpClientConfig, HttpClient httpClient) {
+            super(api, apiKey, apiSecret, callback, scope, state, responseType, userAgent, httpClientConfig, httpClient);
+        }
+
+        @Override
+        protected OAuthRequest createAccessTokenRequest(String code) {
+            final OAuthRequest request = super.createAccessTokenRequest(code);
+            request.addParameter(OAuthConstants.CLIENT_ID, getApiKey());
+            request.addParameter(OAuthConstants.CLIENT_SECRET, getApiSecret());
+            return request;
+        }
+    }
+
+    @Override
+    public OAuth20Service createService(String apiKey, String apiSecret, String callback, String scope, OutputStream debugStream,
+            String state, String responseType, String userAgent, HttpClientConfig httpClientConfig, HttpClient httpClient) {
+        return new Service(this, apiKey, apiSecret, callback, scope, state, responseType, userAgent, httpClientConfig, httpClient);
+    }
+
+    @Override
+    public String getAccessTokenEndpoint() {
+        return "https://api.figshare.com/v2/token";
+    }
+
+    @Override
+    protected String getAuthorizationBaseUrl() {
+        return "https://figshare.com/account/applications/authorize";
+    }
+}
