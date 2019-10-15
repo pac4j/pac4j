60c2161115930aeb9d41da8d503080d0af84af55
Jérôme LELEU <leleuj@gmail.com>
2019-10-15 08:08:37 +0200

    Trying to fix the LDAP address already in use issues

--- pac4j-config/src/test/java/org/pac4j/config/client/PropertiesConfigFactoryTests.java
+++ pac4j-config/src/test/java/org/pac4j/config/client/PropertiesConfigFactoryTests.java
@@ -45,77 +45,78 @@ public final class PropertiesConfigFactoryTests implements TestsConstants {
 
     @Test
     public void test() {
-        final Map<String, String> properties = new HashMap<>();
-        properties.put(FACEBOOK_ID, ID);
-        properties.put(FACEBOOK_SECRET, SECRET);
-        properties.put(TWITTER_ID, ID);
-        properties.put(TWITTER_SECRET, SECRET);
-        properties.put(CAS_LOGIN_URL, CALLBACK_URL);
-        properties.put(CAS_PROTOCOL, CasProtocol.CAS20.toString());
-        properties.put(SAML_KEYSTORE_PASSWORD, PASSWORD);
-        properties.put(SAML_PRIVATE_KEY_PASSWORD, PASSWORD);
-        properties.put(SAML_KEYSTORE_PATH, PATH);
-        properties.put(SAML_IDENTITY_PROVIDER_METADATA_PATH, PATH);
-        properties.put(SAML_AUTHN_REQUEST_BINDING_TYPE, SAMLConstants.SAML2_REDIRECT_BINDING_URI);
-        properties.put(SAML_KEYSTORE_ALIAS, VALUE);
-        properties.put(OIDC_ID, ID);
-        properties.put(OIDC_SECRET, SECRET);
-        properties.put(OIDC_DISCOVERY_URI, CALLBACK_URL);
-        properties.put(OIDC_USE_NONCE, "true");
-        properties.put(OIDC_PREFERRED_JWS_ALGORITHM, "RS384");
-        properties.put(OIDC_MAX_CLOCK_SKEW, "60");
-        properties.put(OIDC_CLIENT_AUTHENTICATION_METHOD, "CLIENT_SECRET_POST");
-        properties.put(OIDC_CUSTOM_PARAM_KEY + "1", KEY);
-        properties.put(OIDC_CUSTOM_PARAM_VALUE + "1", VALUE);
-
-        properties.put(CAS_LOGIN_URL.concat(".1"), LOGIN_URL);
-        properties.put(CAS_PROTOCOL.concat(".1"), CasProtocol.CAS30.toString());
-
-        properties.put(OIDC_TYPE.concat(".1"), "google");
-        properties.put(OIDC_ID.concat(".1"), ID);
-        properties.put(OIDC_SECRET.concat(".1"), SECRET);
-
-        properties.put(ANONYMOUS, "whatever the value");
-
-        properties.put(FORMCLIENT_LOGIN_URL, LOGIN_URL);
-        properties.put(FORMCLIENT_AUTHENTICATOR, "testUsernamePassword");
-
-        properties.put(INDIRECTBASICAUTH_AUTHENTICATOR.concat(".2"), "testUsernamePassword");
-
-        properties.put(LDAP_TYPE, "direct");
-        properties.put(LDAP_URL, "ldap://localhost:" + port);
-        properties.put(LDAP_USE_SSL, "false");
-        properties.put(LDAP_USE_START_TLS, "false");
-        properties.put(LDAP_DN_FORMAT, CN + "=%s," + BASE_PEOPLE_DN);
-        properties.put(LDAP_USERS_DN, BASE_PEOPLE_DN);
-        properties.put(LDAP_PRINCIPAL_ATTRIBUTE_ID, CN);
-        properties.put(LDAP_ATTRIBUTES, SN + "," + ROLE);
-        properties.put(LDAP_PROVIDER_CLASS, JndiProvider.class.getName());
-
-        properties.put(FORMCLIENT_LOGIN_URL.concat(".2"), PAC4J_BASE_URL);
-        properties.put(FORMCLIENT_AUTHENTICATOR.concat(".2"), "ldap");
-
-        properties.put(SPRING_ENCODER_TYPE.concat(".4"), "standard");
-        properties.put(SPRING_ENCODER_STANDARD_SECRET.concat(".4"), SALT);
-
-        properties.put(DB_JDBC_URL, "jdbc:h2:mem:test");
-        properties.put(DB_USERNAME, Pac4jConstants.USERNAME);
-        properties.put(DB_PASSWORD, Pac4jConstants.PASSWORD);
-        properties.put(DB_USERNAME_ATTRIBUTE, Pac4jConstants.USERNAME);
-        properties.put(DB_USER_PASSWORD_ATTRIBUTE, Pac4jConstants.PASSWORD);
-        properties.put(DB_ATTRIBUTES, FIRSTNAME);
-        properties.put(DB_PASSWORD_ENCODER, "encoder.spring.4");
-
-        properties.put(INDIRECTBASICAUTH_AUTHENTICATOR.concat(".5"), "db");
-
-        properties.put(REST_URL.concat(".3"), PAC4J_BASE_URL);
-        properties.put(DIRECTBASICAUTH_AUTHENTICATOR.concat(".7"), "rest.3");
 
         LdapServer ldapServer = null;
         try {
             ldapServer = new LdapServer();
             ldapServer.start();
             new DbServer();
+
+            final Map<String, String> properties = new HashMap<>();
+            properties.put(FACEBOOK_ID, ID);
+            properties.put(FACEBOOK_SECRET, SECRET);
+            properties.put(TWITTER_ID, ID);
+            properties.put(TWITTER_SECRET, SECRET);
+            properties.put(CAS_LOGIN_URL, CALLBACK_URL);
+            properties.put(CAS_PROTOCOL, CasProtocol.CAS20.toString());
+            properties.put(SAML_KEYSTORE_PASSWORD, PASSWORD);
+            properties.put(SAML_PRIVATE_KEY_PASSWORD, PASSWORD);
+            properties.put(SAML_KEYSTORE_PATH, PATH);
+            properties.put(SAML_IDENTITY_PROVIDER_METADATA_PATH, PATH);
+            properties.put(SAML_AUTHN_REQUEST_BINDING_TYPE, SAMLConstants.SAML2_REDIRECT_BINDING_URI);
+            properties.put(SAML_KEYSTORE_ALIAS, VALUE);
+            properties.put(OIDC_ID, ID);
+            properties.put(OIDC_SECRET, SECRET);
+            properties.put(OIDC_DISCOVERY_URI, CALLBACK_URL);
+            properties.put(OIDC_USE_NONCE, "true");
+            properties.put(OIDC_PREFERRED_JWS_ALGORITHM, "RS384");
+            properties.put(OIDC_MAX_CLOCK_SKEW, "60");
+            properties.put(OIDC_CLIENT_AUTHENTICATION_METHOD, "CLIENT_SECRET_POST");
+            properties.put(OIDC_CUSTOM_PARAM_KEY + "1", KEY);
+            properties.put(OIDC_CUSTOM_PARAM_VALUE + "1", VALUE);
+
+            properties.put(CAS_LOGIN_URL.concat(".1"), LOGIN_URL);
+            properties.put(CAS_PROTOCOL.concat(".1"), CasProtocol.CAS30.toString());
+
+            properties.put(OIDC_TYPE.concat(".1"), "google");
+            properties.put(OIDC_ID.concat(".1"), ID);
+            properties.put(OIDC_SECRET.concat(".1"), SECRET);
+
+            properties.put(ANONYMOUS, "whatever the value");
+
+            properties.put(FORMCLIENT_LOGIN_URL, LOGIN_URL);
+            properties.put(FORMCLIENT_AUTHENTICATOR, "testUsernamePassword");
+
+            properties.put(INDIRECTBASICAUTH_AUTHENTICATOR.concat(".2"), "testUsernamePassword");
+
+            properties.put(LDAP_TYPE, "direct");
+            properties.put(LDAP_URL, "ldap://localhost:" + port);
+            properties.put(LDAP_USE_SSL, "false");
+            properties.put(LDAP_USE_START_TLS, "false");
+            properties.put(LDAP_DN_FORMAT, CN + "=%s," + BASE_PEOPLE_DN);
+            properties.put(LDAP_USERS_DN, BASE_PEOPLE_DN);
+            properties.put(LDAP_PRINCIPAL_ATTRIBUTE_ID, CN);
+            properties.put(LDAP_ATTRIBUTES, SN + "," + ROLE);
+            properties.put(LDAP_PROVIDER_CLASS, JndiProvider.class.getName());
+
+            properties.put(FORMCLIENT_LOGIN_URL.concat(".2"), PAC4J_BASE_URL);
+            properties.put(FORMCLIENT_AUTHENTICATOR.concat(".2"), "ldap");
+
+            properties.put(SPRING_ENCODER_TYPE.concat(".4"), "standard");
+            properties.put(SPRING_ENCODER_STANDARD_SECRET.concat(".4"), SALT);
+
+            properties.put(DB_JDBC_URL, "jdbc:h2:mem:test");
+            properties.put(DB_USERNAME, Pac4jConstants.USERNAME);
+            properties.put(DB_PASSWORD, Pac4jConstants.PASSWORD);
+            properties.put(DB_USERNAME_ATTRIBUTE, Pac4jConstants.USERNAME);
+            properties.put(DB_USER_PASSWORD_ATTRIBUTE, Pac4jConstants.PASSWORD);
+            properties.put(DB_ATTRIBUTES, FIRSTNAME);
+            properties.put(DB_PASSWORD_ENCODER, "encoder.spring.4");
+
+            properties.put(INDIRECTBASICAUTH_AUTHENTICATOR.concat(".5"), "db");
+
+            properties.put(REST_URL.concat(".3"), PAC4J_BASE_URL);
+            properties.put(DIRECTBASICAUTH_AUTHENTICATOR.concat(".7"), "rest.3");
 
             final PropertiesConfigFactory factory = new PropertiesConfigFactory(CALLBACK_URL, properties);
             final Config config = factory.build();
--- pac4j-ldap/src/test/java/org/pac4j/ldap/test/tools/LdapServer.java
+++ pac4j-ldap/src/test/java/org/pac4j/ldap/test/tools/LdapServer.java
@@ -3,6 +3,9 @@ package org.pac4j.ldap.test.tools;
 import com.unboundid.ldap.listener.InMemoryDirectoryServer;
 import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig;
 import com.unboundid.ldap.listener.InMemoryListenerConfig;
+import com.unboundid.ldap.sdk.LDAPBindException;
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldif.LDIFException;
 import org.pac4j.core.util.TestsConstants;
 
 /**
@@ -15,49 +18,49 @@ public final class LdapServer implements TestsConstants {
 
     public final static String BASE_DN = "dc=example,dc=com";
     public final static String BASE_PEOPLE_DN = "ou=people,dc=example,dc=com";
-    private final static int PORT = 33389;
-    public final static int port;
+    public static int port;
     public final static String CN = "cn";
     public final static String SN = "sn";
     public final static String ROLE = "role";
     public final static String ROLE1 = "role1";
     public final static String ROLE2 = "role2";
 
-    static {
-        final String sPort = System.getProperty("test.ldap.port");
-        System.out.println("Reserved LDAP port: " + sPort);
-        if (sPort != null) {
-            port = Integer.parseInt(sPort);
-        } else {
-            port = PORT;
-        }
-    }
-
     private InMemoryDirectoryServer ds;
 
     public void start() {
-        try {
-            final InMemoryDirectoryServerConfig dsConfig = new InMemoryDirectoryServerConfig(BASE_DN);
-            dsConfig.setSchema(null);
-            dsConfig.setEnforceAttributeSyntaxCompliance(false);
-            dsConfig.setEnforceSingleStructuralObjectClass(false);
-            dsConfig.setListenerConfigs(new InMemoryListenerConfig("myListener", null, port, null, null, null));
-            dsConfig.addAdditionalBindCredentials(CN + "=" + GOOD_USERNAME + "," + BASE_PEOPLE_DN, PASSWORD);
-            dsConfig.addAdditionalBindCredentials(CN + "=" + GOOD_USERNAME2 + "," + BASE_PEOPLE_DN, PASSWORD);
-            this.ds = new InMemoryDirectoryServer(dsConfig);
-            this.ds.add("dn: " + BASE_DN, "objectClass: organizationalUnit", "objectClass: top");
-            this.ds.add("dn: " + BASE_PEOPLE_DN, "objectClass: organizationalUnit");
-            this.ds.add("dn: " + CN + "=" + GOOD_USERNAME + "," + BASE_PEOPLE_DN, CN + ": " + GOOD_USERNAME, SN + ": "
-                    + FIRSTNAME_VALUE, "objectClass: person");
-            this.ds.add("dn: " + CN + "=" + GOOD_USERNAME2 + "," + BASE_PEOPLE_DN, ROLE + ": " + ROLE1, ROLE + ": " + ROLE2,
-                    "objectClass: person");
-
-            //Debug.setEnabled(true);
-
-            this.ds.startListening();
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
+        for (int testPort = 33389; testPort < 35000; testPort++) {
+            try {
+                port = testPort;
+                startServer(testPort);
+                return;
+            } catch (final LDAPBindException e) {
+                // ignore and try with the next port
+                System.out.println("Port already in use: " + port + ". Trying next one...");
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
         }
+    }
+
+    protected void startServer(final int port) throws LDAPException, LDIFException {
+        final InMemoryDirectoryServerConfig dsConfig = new InMemoryDirectoryServerConfig(BASE_DN);
+        dsConfig.setSchema(null);
+        dsConfig.setEnforceAttributeSyntaxCompliance(false);
+        dsConfig.setEnforceSingleStructuralObjectClass(false);
+        dsConfig.setListenerConfigs(new InMemoryListenerConfig("myListener", null, port, null, null, null));
+        dsConfig.addAdditionalBindCredentials(CN + "=" + GOOD_USERNAME + "," + BASE_PEOPLE_DN, PASSWORD);
+        dsConfig.addAdditionalBindCredentials(CN + "=" + GOOD_USERNAME2 + "," + BASE_PEOPLE_DN, PASSWORD);
+        this.ds = new InMemoryDirectoryServer(dsConfig);
+        this.ds.add("dn: " + BASE_DN, "objectClass: organizationalUnit", "objectClass: top");
+        this.ds.add("dn: " + BASE_PEOPLE_DN, "objectClass: organizationalUnit");
+        this.ds.add("dn: " + CN + "=" + GOOD_USERNAME + "," + BASE_PEOPLE_DN, CN + ": " + GOOD_USERNAME, SN + ": "
+            + FIRSTNAME_VALUE, "objectClass: person");
+        this.ds.add("dn: " + CN + "=" + GOOD_USERNAME2 + "," + BASE_PEOPLE_DN, ROLE + ": " + ROLE1, ROLE + ": " + ROLE2,
+            "objectClass: person");
+
+        //Debug.setEnabled(true);
+
+        this.ds.startListening();
     }
 
     public void stop() {
