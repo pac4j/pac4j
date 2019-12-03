03d1e60d424674f5e3e4a2a31758963cf283b767
Garry Boyce <garry@cambridgesemantics.com>
2019-12-03 18:56:17 -0500

    Fix issue with saml ssocircle

--- pac4j-saml/src/main/java/org/pac4j/saml/transport/AbstractPac4jDecoder.java
+++ pac4j-saml/src/main/java/org/pac4j/saml/transport/AbstractPac4jDecoder.java
@@ -18,9 +18,13 @@ import org.pac4j.core.util.CommonHelper;
 import org.pac4j.saml.context.SAML2MessageContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.Nonnull;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
@@ -62,6 +66,20 @@ public abstract class AbstractPac4jDecoder extends AbstractMessageDecoder<SAMLOb
             throw new MessageDecodingException("Request did not contain either a SAMLRequest parameter, a SAMLResponse parameter, "
                 + "a logoutRequest parameter or a body content");
         } else {
+            MultiValueMap<String, String> paramMap = UriComponentsBuilder.fromUriString("http://dummy.com?" + encodedMessage).build().getQueryParams();
+            String newEncodedMessage = null;
+            for (String parameter : SAML_PARAMETERS) {
+                newEncodedMessage = paramMap.getFirst(parameter);
+                if (newEncodedMessage != null) {
+                    try {
+                        encodedMessage = Optional.of(URLDecoder.decode(newEncodedMessage, StandardCharsets.UTF_8.name()));
+                    } catch (UnsupportedEncodingException e) {
+                        logger.warn("bad decoding:\n{}", e, newEncodedMessage);
+                    }
+                    break;
+                }
+                
+            }
 
             if (encodedMessage.get().contains("<")) {
                 logger.trace("Raw SAML message:\n{}", encodedMessage);
