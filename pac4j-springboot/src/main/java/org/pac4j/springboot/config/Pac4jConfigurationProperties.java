fdef3a750f3561f318a0b21a686fb081d07de187
Misagh Moayyed <mm1844@gmail.com>
2020-03-15 13:32:54 +0330

    initial pass at spring boot

---
+++ pac4j-springboot/src/main/java/org/pac4j/springboot/config/Pac4jConfigurationProperties.java
@@ -0,0 +1,29 @@
+package org.pac4j.springboot.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "pac4j", ignoreUnknownFields = false)
+public class Pac4jConfigurationProperties {
+    private Map<String, String> properties = new LinkedHashMap<>();
+
+    private String callbackUrl;
+
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(final Map<String, String> properties) {
+        this.properties = properties;
+    }
+
+    public String getCallbackUrl() {
+        return callbackUrl;
+    }
+
+    public void setCallbackUrl(final String callbackUrl) {
+        this.callbackUrl = callbackUrl;
+    }
+}
