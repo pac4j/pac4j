5f41f38cc29c100c3e671e25503c7504a664dcc4
Jérôme LELEU <leleuj@gmail.com>
2020-05-04 12:18:06 +0200

    remove final keyword from AnonymousProfile

--- pac4j-core/src/main/java/org/pac4j/core/profile/AnonymousProfile.java
+++ pac4j-core/src/main/java/org/pac4j/core/profile/AnonymousProfile.java
@@ -6,7 +6,7 @@ package org.pac4j.core.profile;
  * @author Jerome Leleu
  * @since 1.8.1
  */
-public final class AnonymousProfile extends CommonProfile {
+public class AnonymousProfile extends CommonProfile {
 
     private static final long serialVersionUID = -7377022639833719511L;
 
