34e5ea355a5d9a395c459aef4854752a2bb6c7e7
Charley Wu <akunzai@gmail.com>
2020-11-04 21:07:41 +0800

    Add Apple OpenID Connect client (#1727)
    - https://developer.apple.com/documentation/sign_in_with_apple
    
    (cherry picked from commit b54c18e343fe2a635d175ff508883cb14dc083a1)

---
+++ pac4j-oidc/src/main/java/org/pac4j/oidc/profile/apple/AppleProfile.java
@@ -0,0 +1,16 @@
+package org.pac4j.oidc.profile.apple;
+
+import org.pac4j.oidc.client.AppleClient;
+import org.pac4j.oidc.profile.OidcProfile;
+
+/**
+ * <p>This class is the user profile for Apple (using OpenID Connect protocol) with appropriate getters.</p>
+ * <p>It is returned by the {@link AppleClient}.</p>
+ *
+ * @author Charley Wu
+ * @since 5.0.0
+ */
+public class AppleProfile extends OidcProfile {
+
+    private static final long serialVersionUID = -2487273611921830541L;
+}
