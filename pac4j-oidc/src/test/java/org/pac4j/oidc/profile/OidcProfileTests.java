ed19245fe4b7a9a35d3350ec6c3ee662ea63452b
jmntn2000 <jmn@provident-solutions.com>
2020-12-04 10:07:44 -0600

    Fix for expired tokens when lifetime isn't used.  issue #1679 (#1748)
    (cherry picked from commit a6813063f3fbab31c486d5c6a1b632db67db72bc)
    
    # Conflicts:
    #	pac4j-oidc/src/test/java/org/pac4j/oidc/profile/OidcProfileTests.java

--- pac4j-oidc/src/test/java/org/pac4j/oidc/profile/OidcProfileTests.java
+++ pac4j-oidc/src/test/java/org/pac4j/oidc/profile/OidcProfileTests.java
@@ -1,5 +1,7 @@
 package org.pac4j.oidc.profile;
 
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.PlainJWT;
 import com.nimbusds.oauth2.sdk.Scope;
 import com.nimbusds.oauth2.sdk.token.AccessToken;
 import com.nimbusds.oauth2.sdk.token.BearerAccessToken;
@@ -8,6 +10,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.pac4j.core.util.JavaSerializationHelper;
 import org.pac4j.core.util.TestsConstants;
+
+import java.time.Instant;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -178,5 +183,23 @@ public final class OidcProfileTests implements TestsConstants {
         profile.setAccessToken(token);
         profile.setTokenExpirationAdvance(3600); // 1 hour
         assertTrue(profile.isExpired());
+    }
+
+    /**
+     * Test experation based on access token exp date.
+     */
+    @Test
+    public void testAccessTokenExpiration(){
+
+        final OidcProfile profile = new OidcProfile();
+        profile.setAccessToken(new BearerAccessToken(ID_TOKEN));
+
+        assertTrue(profile.isExpired());
+
+        JWTClaimsSet cs = new JWTClaimsSet.Builder().expirationTime(Date.from(Instant.now().plusSeconds(30))).build();
+
+        profile.setAccessToken(new BearerAccessToken(new PlainJWT(cs).serialize()));
+
+        assertFalse(profile.isExpired());
     }
 }
