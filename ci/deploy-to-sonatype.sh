4bf960a1beb3e12cdfb0adc2342d211ac35cf422
Jérôme LELEU <leleuj@gmail.com>
2021-02-05 16:50:56 +0100

    github actions: deploy test

---
+++ ci/deploy-to-sonatype.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "GITHUB_HEAD_REF: ${GITHUB_HEAD_REF}"
+echo -e "GITHUB_REF: ${GITHUB_REF}"
+
+# Only invoke the deployment to Sonatype when it's not a PR and only for main branches
+if [ "$GITHUB_HEAD_REF" == "" ] && [[ "$GITHUB_REF" =~ ^(master|.*\.x)$ ]]; then
+  echo -e "Deploying snapshot artifacts to Sonatype under job ${GITHUB_RUN_ID}"
+  mvn deploy -DskipTests
+  echo -e "Successfully deployed snapshot artifacts to Sonatype under job ${GITHUB_RUN_ID}"
+fi
