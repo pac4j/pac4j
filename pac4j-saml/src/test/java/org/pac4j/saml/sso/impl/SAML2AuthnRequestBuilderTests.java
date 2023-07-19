56a16c346f3df4b82172e91cfdeff84fa4ebfe69
Misagh Moayyed <mm1844@gmail.com>
2023-07-19 15:26:35 +0400

    allow oidc metadata resolver to support hostname verification

--- pac4j-saml/src/test/java/org/pac4j/saml/sso/impl/SAML2AuthnRequestBuilderTests.java
+++ pac4j-saml/src/test/java/org/pac4j/saml/sso/impl/SAML2AuthnRequestBuilderTests.java
@@ -5,16 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.opensaml.saml.common.xml.SAMLConstants;
 import org.opensaml.saml.saml2.core.AuthnRequest;
-import org.opensaml.saml.saml2.core.Issuer;
-import org.opensaml.saml.saml2.core.LogoutRequest;
-import org.opensaml.saml.saml2.core.NameID;
-import org.opensaml.saml.saml2.core.SessionIndex;
 import org.opensaml.saml.saml2.metadata.AssertionConsumerService;
 import org.opensaml.saml.saml2.metadata.IDPSSODescriptor;
 import org.opensaml.saml.saml2.metadata.SPSSODescriptor;
 import org.opensaml.saml.saml2.metadata.SingleSignOnService;
 import org.pac4j.core.context.CallContext;
 import org.pac4j.core.context.MockWebContext;
+import org.pac4j.core.context.WebContext;
 import org.pac4j.core.context.session.MockSessionStore;
 import org.pac4j.core.redirect.RedirectionActionBuilder;
 import org.pac4j.saml.client.AbstractSAML2ClientTests;
@@ -22,14 +19,10 @@ import org.pac4j.saml.client.SAML2Client;
 import org.pac4j.saml.config.SAML2Configuration;
 import org.pac4j.saml.context.SAML2MessageContext;
 import org.pac4j.saml.profile.api.SAML2ObjectBuilder;
-import org.pac4j.saml.util.Configuration;
 
-import java.time.Clock;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.Collections;
-import java.util.UUID;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -54,52 +47,22 @@ public class SAML2AuthnRequestBuilderTests extends AbstractSAML2ClientTests {
     }
 
     @Test
-    public void testHttpSessionStoreGetterAndSetter() throws Exception {
-//        final WebContext webContext = MockWebContext.create();
-//
-//        val messageStoreFactory = configuration.getSamlMessageStoreFactory();
-//        val store = messageStoreFactory.getMessageStore(webContext, new MockSessionStore());
-//
-//        SAML2ObjectBuilder<AuthnRequest> builder = new SAML2AuthnRequestBuilder();
+    public void testHttpSessionStoreGetterAndSetter() {
+        final WebContext webContext = MockWebContext.create();
+
+        val messageStoreFactory = configuration.getSamlMessageStoreFactory();
+        val store = messageStoreFactory.getMessageStore(webContext, new MockSessionStore());
+
+        SAML2ObjectBuilder<AuthnRequest> builder = new SAML2AuthnRequestBuilder();
         val context = buildContext();
 
-//        val authnRequest = builder.build(context);
-//        authnRequest.setAssertionConsumerServiceURL("https://pac4j.org");
-//
-//        store.set(authnRequest.getID(), authnRequest);
-//
-//        assertNotNull(store.get(authnRequest.getID()));
-//        assertEquals("https://pac4j.org", authnRequest.getAssertionConsumerServiceURL());
+        val authnRequest = builder.build(context);
+        authnRequest.setAssertionConsumerServiceURL("https://pac4j.org");
 
+        store.set(authnRequest.getID(), authnRequest);
 
-        var logoutRequest = (LogoutRequest) Configuration.getBuilderFactory()
-            .getBuilder(LogoutRequest.DEFAULT_ELEMENT_NAME)
-            .buildObject(LogoutRequest.DEFAULT_ELEMENT_NAME);
-
-        logoutRequest.setID("23hgbcehfgeb7843jdv1");
-        val issuer =  (Issuer) Configuration.getBuilderFactory()
-            .getBuilder(Issuer.DEFAULT_ELEMENT_NAME)
-            .buildObject(Issuer.DEFAULT_ELEMENT_NAME);
-
-        issuer.setValue("https://samltest.id/saml/sp");
-
-        var n = (NameID) Configuration.getBuilderFactory()
-            .getBuilder(NameID.DEFAULT_ELEMENT_NAME)
-            .buildObject(NameID.DEFAULT_ELEMENT_NAME);
-        n.setValue(UUID.randomUUID().toString());
-        logoutRequest.setNameID(n);
-
-        var s = (SessionIndex) Configuration.getBuilderFactory()
-            .getBuilder(SessionIndex.DEFAULT_ELEMENT_NAME)
-            .buildObject(SessionIndex.DEFAULT_ELEMENT_NAME);
-        s.setValue(UUID.randomUUID().toString());
-        logoutRequest.getSessionIndexes().add(s);
-
-        logoutRequest.setIssuer(issuer);
-        logoutRequest.setDestination("https://localhost:8443/cas/idp/profile/SAML2/POST/SLO");
-        logoutRequest.setIssueInstant(Instant.now(Clock.systemUTC()).plus(Duration.ofDays(365)));
-
-        new SAML2Client(configuration).getLogoutRequestMessageSender().sendMessage(context, logoutRequest, "");
+        assertNotNull(store.get(authnRequest.getID()));
+        assertEquals("https://pac4j.org", authnRequest.getAssertionConsumerServiceURL());
     }
 
     private SAML2MessageContext buildContext() {
