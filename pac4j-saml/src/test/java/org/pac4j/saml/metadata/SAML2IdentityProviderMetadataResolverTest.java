eb705c32f91b25e98d477b7c81877babbd2eebee
Jérôme LELEU <leleuj@gmail.com>
2024-05-23 08:49:33 +0200

    fix test

--- pac4j-saml/src/test/java/org/pac4j/saml/metadata/SAML2IdentityProviderMetadataResolverTest.java
+++ pac4j-saml/src/test/java/org/pac4j/saml/metadata/SAML2IdentityProviderMetadataResolverTest.java
@@ -3,7 +3,6 @@ package org.pac4j.saml.metadata;
 import net.shibboleth.shared.resolver.CriteriaSet;
 import net.shibboleth.shared.xml.XMLParserException;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.opensaml.core.criterion.EntityIdCriterion;
 import org.pac4j.core.exception.TechnicalException;
@@ -13,7 +12,6 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.UrlResource;
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.net.InetSocketAddress;
@@ -73,34 +71,12 @@ public class SAML2IdentityProviderMetadataResolverTest {
     }
 
     @Test
-    @Ignore
-    public void resolveMetadataOverUrlWithHostnameVerifierFromConfig() throws Exception {
+    public void resolveMetadataOverUrlWithHostnameVerifier() throws Exception {
         var configuration = new SAML2Configuration();
-        configuration.setIdentityProviderMetadataResource(new UrlResource("https://self-signed.badssl.com"));
+        configuration.setIdentityProviderMetadataResource(new UrlResource("https://www.pac4j.org"));
         configuration.setHostnameVerifier((s, sslSession) -> true);
         configuration.setSslSocketFactory(disabledSslContext().getSocketFactory());
         metadataResolver = new SAML2IdentityProviderMetadataResolver(configuration);
-        try {
-            metadataResolver.init();
-        } catch (final TechnicalException e) {
-            assertEquals(XMLParserException.class, e.getCause().getClass());
-        }
-    }
-
-    @Test
-    @Ignore
-    public void resolveMetadataOverUrlWithHostnameVerifier() throws Exception {
-        var configuration = new SAML2Configuration();
-        configuration.setIdentityProviderMetadataResource(new UrlResource("https://self-signed.badssl.com"));
-        metadataResolver = new SAML2IdentityProviderMetadataResolver(configuration);
-        try {
-            metadataResolver.init();
-        } catch (final TechnicalException e) {
-            assertEquals(SSLHandshakeException.class, e.getCause().getClass());
-        }
-
-        metadataResolver.setHostnameVerifier((s, sslSession) -> true);
-        metadataResolver.setSslSocketFactory(disabledSslContext().getSocketFactory());
         try {
             metadataResolver.init();
         } catch (final TechnicalException e) {
