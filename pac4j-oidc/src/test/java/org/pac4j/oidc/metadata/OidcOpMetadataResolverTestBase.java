197ce6c76366fd13c80d5b464c0b5475ae71e8b9
Mathias Loesch <m.loesch@celonis.com>
2024-05-29 15:43:22 +0200

    fix: StaticOidcOpMetadataResolver does not enforce discovery URI
    StaticOidcOpMetadataResolver returns the static OIDC metadata
    provided during initialization and therefore should not require
    a metadata discovery URI on the OIDC configuration object.

---
+++ pac4j-oidc/src/test/java/org/pac4j/oidc/metadata/OidcOpMetadataResolverTestBase.java
@@ -0,0 +1,49 @@
+package org.pac4j.oidc.metadata;
+
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.oauth2.sdk.auth.ClientAuthenticationMethod;
+import com.nimbusds.oauth2.sdk.id.Issuer;
+import com.nimbusds.openid.connect.sdk.SubjectType;
+import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Set;
+import org.pac4j.oidc.config.OidcConfiguration;
+
+/**
+ * @author Mathias Loesch
+ * @since 6.0.4
+ */
+public class OidcOpMetadataResolverTestBase {
+
+    public static final JWSAlgorithm JWS_ALGORITHM = JWSAlgorithm.HS256;
+
+    protected static OidcConfiguration getOidcConfiguration(Set<ClientAuthenticationMethod> supportedClientAuthenticationMethods,
+        String discoveryURI) {
+        OidcConfiguration configuration = new OidcConfiguration();
+        configuration.setClientId("clientId");
+        configuration.setSecret("secret");
+        configuration.setDiscoveryURI(discoveryURI);
+        configuration.setPreferredJwsAlgorithm(OidcOpMetadataResolverTestBase.JWS_ALGORITHM);
+        configuration.setSupportedClientAuthenticationMethods(supportedClientAuthenticationMethods);
+        return configuration;
+    }
+
+    protected static StaticOidcOpMetadataResolver getStaticMetadataResolver(OidcConfiguration configuration,
+        List<ClientAuthenticationMethod> supportedAuthMethods) throws URISyntaxException {
+        OIDCProviderMetadata providerMetadata = OidcOpMetadataResolverTestBase.getOidcProviderMetadata(supportedAuthMethods);
+        StaticOidcOpMetadataResolver oidcOpMetadataResolver = new StaticOidcOpMetadataResolver(configuration, providerMetadata);
+
+        oidcOpMetadataResolver.init();
+        return oidcOpMetadataResolver;
+    }
+
+    protected static OIDCProviderMetadata getOidcProviderMetadata(List<ClientAuthenticationMethod> supportedClientAuthenticationMethods)
+        throws URISyntaxException {
+        OIDCProviderMetadata providerMetadata = new OIDCProviderMetadata(new Issuer("issuer"), List.of(SubjectType.PUBLIC), new URI(""));
+        providerMetadata.setIDTokenJWSAlgs(List.of(OidcOpMetadataResolverTestBase.JWS_ALGORITHM));
+        providerMetadata.setTokenEndpointAuthMethods(supportedClientAuthenticationMethods);
+        return providerMetadata;
+    }
+}
