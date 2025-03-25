aacb494fbc41656a6a5cc45f5b550f9ee55aebd0
Misagh Moayyed <mm1844@gmail.com>
2025-03-25 17:19:34 +0400

    feat: Resolve saml2 metadata url concurrently via ranges (#3342)
    * feat: resolve saml2 metadata url concurrently via ranges
    
    * feat: resolve saml2 metadata url concurrently via ranges
    
    * feat: resolve saml2 metadata url concurrently via ranges
    
    * feat: resolve saml2 metadata url concurrently via ranges
    
    * feat: resolve saml2 metadata url concurrently via ranges
    
    * feat: resolve saml2 metadata url concurrently via ranges
    
    * feat: resolve saml2 metadata url concurrently via ranges

--- pac4j-saml/src/main/java/org/pac4j/saml/metadata/SAML2IdentityProviderMetadataResolver.java
+++ pac4j-saml/src/main/java/org/pac4j/saml/metadata/SAML2IdentityProviderMetadataResolver.java
@@ -11,23 +11,37 @@ import org.opensaml.core.criterion.EntityIdCriterion;
 import org.opensaml.core.xml.XMLObject;
 import org.opensaml.saml.metadata.IterableMetadataSource;
 import org.opensaml.saml.metadata.resolver.MetadataResolver;
+import org.opensaml.saml.metadata.resolver.filter.impl.EntityRoleFilter;
 import org.opensaml.saml.metadata.resolver.impl.DOMMetadataResolver;
 import org.opensaml.saml.metadata.resolver.index.impl.RoleMetadataIndex;
 import org.opensaml.saml.saml2.metadata.EntitiesDescriptor;
 import org.opensaml.saml.saml2.metadata.EntityDescriptor;
+import org.opensaml.saml.saml2.metadata.IDPSSODescriptor;
 import org.pac4j.core.exception.TechnicalException;
 import org.pac4j.core.resource.SpringResourceHelper;
 import org.pac4j.core.resource.SpringResourceLoader;
 import org.pac4j.saml.config.SAML2Configuration;
 import org.pac4j.saml.exceptions.SAMLException;
 import org.pac4j.saml.util.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 
 import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
 import java.net.Proxy;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Resolve and download idp metadata to form a metadata resolver.
@@ -49,11 +63,6 @@ public class SAML2IdentityProviderMetadataResolver extends SpringResourceLoader<
 
     private final SAML2Configuration configuration;
 
-    /**
-     * <p>Constructor for SAML2IdentityProviderMetadataResolver.</p>
-     *
-     * @param configuration a {@link SAML2Configuration} object
-     */
     public SAML2IdentityProviderMetadataResolver(final SAML2Configuration configuration) {
         super(configuration.getIdentityProviderMetadataResource());
         if (configuration.getSslSocketFactory() != null) {
@@ -73,21 +82,107 @@ public class SAML2IdentityProviderMetadataResolver extends SpringResourceLoader<
         return load();
     }
 
-    /**
-     * <p>internalLoad.</p>
-     */
     protected void internalLoad() {
-        this.loaded = initializeMetadataResolver();
+        val t0 = System.currentTimeMillis();
+        try {
+            this.loaded = initializeMetadataResolver();
+        } finally {
+            val t1 = System.currentTimeMillis();
+            LOGGER.debug("Metadata resolution took: {} ms", t1 - t0);
+        }
+
     }
 
-    /**
-     * <p>initializeMetadataResolver.</p>
-     *
-     * @return a {@link DOMMetadataResolver} object
-     */
     protected DOMMetadataResolver initializeMetadataResolver() {
+        if (configuration.getIdentityProviderMetadataResource() instanceof UrlResource urlResource) {
+            var fileUrl = urlResource.getURL().toString();
+            HttpURLConnection conn = null;
+            try {
+                conn = (HttpURLConnection) new URL(fileUrl).openConnection();
+                if (conn instanceof HttpsURLConnection https) {
+                    if (hostnameVerifier != null) {
+                        https.setHostnameVerifier(hostnameVerifier);
+                    }
+                    if (sslSocketFactory != null) {
+                        https.setSSLSocketFactory(sslSocketFactory);
+                    }
+                }
+                conn.setRequestMethod("HEAD");
+                var supportsRange = "bytes".equalsIgnoreCase(conn.getHeaderField("Accept-Ranges"));
+                if (supportsRange) {
+                    var contentLength = conn.getContentLengthLong();
+                    conn.disconnect();
+                    return downloadMetadata(contentLength, fileUrl);
+                }
+            } catch (Exception e) {
+                throw new TechnicalException("Error getting idp metadata resource", e);
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
+        }
+
+        return loadMetadataFromResource(configuration.getIdentityProviderMetadataResource());
+    }
+
+    private DOMMetadataResolver downloadMetadata(final long contentLength, final String url) {
+        var numThreads = Runtime.getRuntime().availableProcessors();
+
+        var partSize = contentLength / numThreads;
+        var executor = Executors.newFixedThreadPool(numThreads);
+        try {
+            var destination = Files.createTempFile("idpmetadata", ".xml").toFile();
+            LOGGER.debug("Downloading idp metadata to {}", destination.getAbsolutePath());
+            var futures = new ArrayList<Future<Void>>();
+
+            for (var i = 0; i < numThreads; i++) {
+                var start = i * partSize;
+                var end = i == numThreads - 1 ? contentLength - 1 : start + partSize - 1;
+
+                futures.add(executor.submit(() -> {
+                    var conn = (HttpURLConnection) new URL(url).openConnection();
+                    if (conn instanceof HttpsURLConnection https) {
+                        if (hostnameVerifier != null) {
+                            https.setHostnameVerifier(hostnameVerifier);
+                        }
+                        if (sslSocketFactory != null) {
+                            https.setSSLSocketFactory(sslSocketFactory);
+                        }
+                    }
+                    conn.setRequestProperty("Range", "bytes=" + start + '-' + end);
+
+                    try (var in = conn.getInputStream(); var out = new RandomAccessFile(destination, "rw")) {
+                        out.seek(start);
+                        var buffer = new byte[8192];
+                        var bytesRead = 0;
+                        while ((bytesRead = in.read(buffer)) != -1) {
+                            out.write(buffer, 0, bytesRead);
+                        }
+                    } finally {
+                        conn.disconnect();
+                    }
+                    return null;
+                }));
+            }
+
+            for (var future : futures) {
+                future.get();
+            }
+            executor.shutdown();
+
+            LOGGER.debug("Finished downloading idp metadata to {}", destination.getAbsolutePath());
+            var resource = new FileSystemResource(destination);
+            return loadMetadataFromResource(resource);
+        } catch (Exception e) {
+            throw new TechnicalException("Error downloading idp metadata", e);
+        }
+    }
+
+    private DOMMetadataResolver loadMetadataFromResource(final Resource resource) {
+        LOGGER.debug("Loading idp metadata from {}", resource);
         try (var in = SpringResourceHelper.getResourceInputStream(
-            configuration.getIdentityProviderMetadataResource(),
+            resource,
             proxy,
             sslSocketFactory,
             hostnameVerifier,
@@ -102,6 +197,11 @@ public class SAML2IdentityProviderMetadataResolver extends SpringResourceLoader<
             resolver.setFailFastInitialization(true);
             resolver.setRequireValidMetadata(true);
             resolver.setId(resolver.getClass().getCanonicalName());
+
+            var entityRoleFilter = new EntityRoleFilter(List.of(IDPSSODescriptor.DEFAULT_ELEMENT_NAME));
+            entityRoleFilter.initialize();
+
+            resolver.setMetadataFilter(entityRoleFilter);
             resolver.initialize();
             return resolver;
         } catch (final FileNotFoundException e) {
@@ -136,7 +236,6 @@ public class SAML2IdentityProviderMetadataResolver extends SpringResourceLoader<
         return idpEntityId;
     }
 
-    /** {@inheritDoc} */
     @Override
     public String getEntityId() {
         val md = getEntityDescriptorElement();
@@ -149,7 +248,6 @@ public class SAML2IdentityProviderMetadataResolver extends SpringResourceLoader<
         throw new SAMLException("No idp entityId found");
     }
 
-    /** {@inheritDoc} */
     @Override
     public String getMetadata() {
         if (getEntityDescriptorElement() != null) {
@@ -158,7 +256,9 @@ public class SAML2IdentityProviderMetadataResolver extends SpringResourceLoader<
         throw new TechnicalException("Metadata cannot be retrieved because entity descriptor is null");
     }
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final XMLObject getEntityDescriptorElement() {
         try {
