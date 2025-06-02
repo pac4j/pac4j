207094ef5a6351f7f1c2b866acd76ed871c35ef6
Misagh Moayyed <mm1844@gmail.com>
2025-06-02 12:28:35 +0400

    Allow SAML2 idp url resource to consider SAML2 connection config (#3430)
    * allow saml idp url resource to consider saml2 connection config
    
    * allow saml idp url resource to consider saml2 connection config
    
    * allow saml idp url resource to consider saml2 connection config
    
    * allow saml idp url resource to consider saml2 connection config

---
+++ pac4j-saml/src/main/java/org/pac4j/saml/util/SAML2UrlResource.java
@@ -0,0 +1,39 @@
+package org.pac4j.saml.util;
+
+import lombok.EqualsAndHashCode;
+import org.pac4j.saml.config.SAML2Configuration;
+import org.springframework.core.io.UrlResource;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * This is {@link SAML2UrlResource}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.1.4
+ */
+@EqualsAndHashCode(callSuper = true)
+public class SAML2UrlResource extends UrlResource {
+    private final SAML2Configuration saml2Configuration;
+
+    public SAML2UrlResource(final URL url, final SAML2Configuration saml2Configuration) {
+        super(url);
+        this.saml2Configuration = saml2Configuration;
+    }
+
+    @Override
+    protected void customizeConnection(final HttpURLConnection connection) throws IOException {
+        super.customizeConnection(connection);
+        if (connection instanceof HttpsURLConnection httpsURLConnection) {
+            if (saml2Configuration.getSslSocketFactory() != null) {
+                httpsURLConnection.setSSLSocketFactory(saml2Configuration.getSslSocketFactory());
+            }
+            if (saml2Configuration.getHostnameVerifier() != null) {
+                httpsURLConnection.setHostnameVerifier(saml2Configuration.getHostnameVerifier());
+            }
+        }
+    }
+}
