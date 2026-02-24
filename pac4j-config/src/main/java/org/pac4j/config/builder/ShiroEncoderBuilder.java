d23d7c3ded4f870389c2de08aa553af01a231773
Jérôme LELEU <leleuj@gmail.com>
2026-02-24 12:33:47 +0100

    Update to Shiro v2.1

--- pac4j-config/src/main/java/org/pac4j/config/builder/ShiroEncoderBuilder.java
+++ pac4j-config/src/main/java/org/pac4j/config/builder/ShiroEncoderBuilder.java
@@ -4,10 +4,11 @@ import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.authc.credential.DefaultPasswordService;
 import org.apache.shiro.crypto.hash.DefaultHashService;
-import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.lang.util.ByteSource;
 import org.pac4j.core.credentials.password.PasswordEncoder;
 import org.pac4j.core.credentials.password.ShiroPasswordEncoder;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -45,18 +46,31 @@ public class ShiroEncoderBuilder extends AbstractBuilder {
                 if (hasProperty) {
                     val hashService = new DefaultHashService();
 
+                    val parameters = new HashMap<String, Object>();
+
                     if (containsProperty(SHIRO_ENCODER_GENERATE_PUBLIC_SALT, i)) {
-                        hashService.setGeneratePublicSalt(getPropertyAsBoolean(SHIRO_ENCODER_GENERATE_PUBLIC_SALT, i));
+                        parameters.put("generatePublicSalt", getPropertyAsBoolean(SHIRO_ENCODER_GENERATE_PUBLIC_SALT, i));
+                    } else {
+                        // deprecated
+                        parameters.put("generatePublicSalt", true);
                     }
                     if (containsProperty(SHIRO_ENCODER_HASH_ALGORITHM_NAME, i)) {
-                        hashService.setHashAlgorithmName(getProperty(SHIRO_ENCODER_HASH_ALGORITHM_NAME, i));
+                        hashService.setDefaultAlgorithmName(getProperty(SHIRO_ENCODER_HASH_ALGORITHM_NAME, i));
+                    } else {
+                        // deprecated
+                        hashService.setDefaultAlgorithmName("SHA-256");
                     }
                     if (containsProperty(SHIRO_ENCODER_HASH_ITERATIONS, i)) {
-                        hashService.setHashIterations(getPropertyAsInteger(SHIRO_ENCODER_HASH_ITERATIONS, i));
+                        parameters.put("hashIterations", getPropertyAsInteger(SHIRO_ENCODER_HASH_ITERATIONS, i));
+                    } else {
+                        // deprecated
+                        parameters.put("hashIterations", 500000);
                     }
                     if (containsProperty(SHIRO_ENCODER_PRIVATE_SALT, i)) {
-                        hashService.setPrivateSalt(ByteSource.Util.bytes(getProperty(SHIRO_ENCODER_PRIVATE_SALT, i)));
+                        parameters.put("privateSalt", ByteSource.Util.bytes(getProperty(SHIRO_ENCODER_PRIVATE_SALT, i)));
                     }
+
+                    hashService.setParameters(parameters);
 
                     passwordService.setHashService(hashService);
                 }
