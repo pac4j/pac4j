8a952da91af698110ec577724bced9bf1d98f92b
Jérôme LELEU <leleuj@gmail.com>
2026-04-08 15:37:54 +0200

    update Announc

--- pac4j-core/src/main/java/org/pac4j/core/util/Announce.java
+++ pac4j-core/src/main/java/org/pac4j/core/util/Announce.java
@@ -16,10 +16,6 @@ public final class Announce {
     public Announce() {
         if (!displayed) {
             LOGGER.warn("\u26A0 UPCOMING VERSIONS:");
-            LOGGER.warn("\u26A0 + v6.5.0:");
-            LOGGER.warn("\u26A0  - the `pac4j-gae`, `pac4j-couch` and `pac4j-springboot` modules will be removed");
-            LOGGER.warn("\u26A0  - the OSGi and shading Maven phases will be removed");
-            LOGGER.warn("\u26A0  - the JSON serializer is the default one for `(Ldap|Db|Mongo)ProfileService`");
             LOGGER.warn("\u26A0 + v7.0.0:");
             LOGGER.warn("\u26A0  - the `pac4j-config` module will be removed");
             LOGGER.warn("\u26A0  - the `pac4j-javaee` module will likely be removed");
