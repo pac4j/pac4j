be17abeeff003c53385fbc77104365c6deccb302
Jérôme LELEU <leleuj@gmail.com>
2026-08-26 16:24:26 +0200

    dep

--- pac4j-core/src/main/java/org/pac4j/test/mongo/MongoServer.java
+++ pac4j-core/src/main/java/org/pac4j/test/mongo/MongoServer.java
@@ -1,11 +1,11 @@
 package org.pac4j.test.mongo;
 
+import de.flapdoodle.commons.reverse.TransitionWalker;
+import de.flapdoodle.commons.reverse.transitions.Start;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.mongo.transitions.Mongod;
 import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess;
-import de.flapdoodle.reverse.TransitionWalker;
-import de.flapdoodle.reverse.transitions.Start;
 import org.pac4j.test.util.TestsConstants;
 
 /**
